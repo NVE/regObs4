30068fd2ae1558cbe2c9f768b88d2ebef4a1a2cb
ingljo <ingljo@gmail.com>
2018-08-24 15:22:49 +0200

    Added opentopomap when outside norway

--- src/settings.ts
+++ src/settings.ts
@@ -20,5 +20,30 @@ export const settings = {
             dbName: 'nanoSql',
             tableName: 'registration'
         }
+    },
+    map: {
+        tiles: {
+            embeddedUrl: '/assets/map/topo_{z}_{x}_{y}.jpg',
+            defaultMapUrl: 'http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norgeskart_bakgrunn&zoom={z}&x={x}&y={y}',
+            fallbackMapUrl: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+            supportTiles: {
+                Snow: [{
+                    name: 'steepness', description: 'STEEPNESS_MAP_DESCRIPTION',
+                    url: 'http://gis3.nve.no/arcgis/rest/services/wmts/Bratthet/MapServer/tile/{z}/{y}/{x}'
+                }],
+                Dirt: [{
+                    name: 'quickclay', description: 'CLAY_ZONES_MAP_DESCRIPTION',
+                    url: 'http://gis3.nve.no/arcgis/rest/services/wmts/Flomsoner1/MapServer/tile/{z}/{y}/{x}'
+                }],
+                Water: [{
+                    name: 'floodzoones', description: 'FLOOD_ZONES_MAP_DESCRIPTION',
+                    url: 'http://gis2.nve.no/arcgis/rest/services/wmts/Kvikkleire_Jordskred/MapServer/tile/{z}/{y}/{x}'
+                }],
+                Ice: [{
+                    name: 'weakened_ice', description: 'WEAKENED_ICE_MAP_DESCRIPTION',
+                    url: 'http://gis3.nve.no/arcgis/rest/services/wmts/SvekketIs/MapServer/tile/{z}/{y}/{x}'
+                }]
+            }
+        }
     }
 };
