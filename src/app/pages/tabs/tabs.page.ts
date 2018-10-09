552c738ac8b42e68c0d130274ac07a9e203c1cf8
ingljo <ingljo@gmail.com>
2018-10-09 13:58:04 +0200

    Added warning badge to tabs

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,6 +1,9 @@
-import { Component, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { Events, Tabs } from '@ionic/angular';
 import { settings } from '../../../settings';
+import { Subscription } from 'rxjs';
+import { WarningService } from '../../core/services/warning/warning.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tabs',
@@ -8,10 +11,11 @@ import { settings } from '../../../settings';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage implements OnInit, OnDestroy {
-
+  private subscription: Subscription;
+  warningsInView: { count: number; text: string, maxWarning: number };
   fullscreen = false;
   @ViewChild(Tabs) private tabs: Tabs;
-  constructor(private events: Events) {
+  constructor(private events: Events, private warningService: WarningService) {
 
   }
 
@@ -19,10 +23,40 @@ export class TabsPage implements OnInit, OnDestroy {
     this.events.subscribe(settings.events.fullscreenChanged, (isFullscreen) => {
       this.fullscreen = isFullscreen;
     });
+    this.subscription = this.warningService.warningGroupInMapViewObservable$.pipe(map((warningsInView) => {
+      const allWarnings = [...warningsInView.center, ...warningsInView.viewBounds];
+      return {
+        count: allWarnings.length,
+        text: allWarnings.length > 9 ? '9+' : allWarnings.length.toString(),
+        maxWarning: Math.max(
+          ...(allWarnings.map((g) => {
+            const maxFromWarnings = g.warnings.map((w) => w.warningLevel);
+            return Math.max(...maxFromWarnings);
+          }))
+        ),
+      };
+    })).subscribe((val) => {
+      this.warningsInView = val;
+    });
+  }
+
+  getBadge() {
+    return (this.warningsInView && this.warningsInView.maxWarning > 0) ? this.warningsInView.text : '';
+  }
+
+  getBadgeColor() {
+    if (this.warningsInView && this.warningsInView.maxWarning > 0) {
+      // return 'warninglevel-' + this.warningsInView.maxWarning;
+      // TODO: Comment in when bug is fixed: https://github.com/ionic-team/ionic/issues/14840
+      return '';
+    } else {
+      return '';
+    }
   }
 
   ngOnDestroy(): void {
     this.events.unsubscribe(settings.events.fullscreenChanged);
+    this.subscription.unsubscribe();
   }
 
   tabsChanged(event: CustomEvent) {
