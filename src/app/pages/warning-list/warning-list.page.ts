847b0b123b63e485016480e30d7abb2ac01642bc
ingljo <ingljo@gmail.com>
2018-10-10 14:33:56 +0200

    Added translations to warning list and show/hide map center items

--- src/app/pages/warning-list/warning-list.page.ts
+++ src/app/pages/warning-list/warning-list.page.ts
@@ -6,6 +6,8 @@ import { map, tap } from 'rxjs/operators';
 import * as moment from 'moment';
 import { WarningGroup } from '../../core/services/warning/warning-group.model';
 import { IWarningGroupInMapView } from '../../core/services/warning/warninggroup-in-mapview.interface';
+import { UserSetting } from '../../core/models/user-settings.model';
+import { UserSettingService } from '../../core/services/user-setting/user-setting.service';
 
 @Component({
   selector: 'app-warning-list',
@@ -17,15 +19,18 @@ export class WarningListPage implements OnInit, OnDestroy {
   warningsForCurrentGeoHazard$: Observable<WarningGroup[]>;
   warningsInMapView$: Observable<IWarningGroupInMapView>;
   favourites$: Observable<WarningGroup[]>;
+  userSettings$: Observable<UserSetting>;
 
   constructor(
     private warningService: WarningService,
+    private userSettingService: UserSettingService,
     private events: Events, private zone: NgZone) {
   }
 
   async ngOnInit() {
     this.selectedTab = 'inMapView';
     this.warningsInMapView$ = this.warningService.warningGroupInMapViewObservable$;
+    this.userSettings$ = this.userSettingService.userSettingObservable$;
   }
 
   selectTab(tab: 'inMapView' | 'all' | 'favourites') {
@@ -38,6 +43,14 @@ export class WarningListPage implements OnInit, OnDestroy {
           warningGroups.filter((wg) => wg.isFavourite)));
     }
     this.selectedTab = tab;
+  }
+
+  getViewBoundItems(warnings: IWarningGroupInMapView, userSettings: UserSetting) {
+    if (userSettings.showMapCenter) {
+      return warnings.viewBounds;
+    } else {
+      return [...warnings.center, ...warnings.viewBounds];
+    }
   }
 
   getDayName(daysToAdd: number) {
