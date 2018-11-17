6ba83824fe8805da9784057ba229c02a65330b30
ingljo <ingljo@gmail.com>
2018-11-17 13:30:45 +0100

    Added pinch to zoom

--- src/app/pages/fullscreen-image-modal/fullscreen-image-modal.module.ts
+++ src/app/pages/fullscreen-image-modal/fullscreen-image-modal.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { FullscreenImageModalPage } from './fullscreen-image-modal.page';
+import { PinchZoomModule } from 'ngx-pinch-zoom';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, FormsModule],
+  imports: [CommonModule, IonicModule, FormsModule, PinchZoomModule],
   declarations: [FullscreenImageModalPage],
   entryComponents: [FullscreenImageModalPage]
 })
--- src/app/pages/fullscreen-image-modal/fullscreen-image-modal.page.ts
+++ src/app/pages/fullscreen-image-modal/fullscreen-image-modal.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
