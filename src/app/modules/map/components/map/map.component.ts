06d6c485ed9c6a87d056c657c8e7b8eb6eff6fa8
ingljo <ingljo@gmail.com>
2018-11-19 13:42:23 +0100

    Fixed missing setting

--- src/app/modules/map/components/map/map.component.ts
+++ src/app/modules/map/components/map/map.component.ts
@@ -52,7 +52,7 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit {
   ) { }
 
   options: L.MapOptions = {
-    zoom: this.zoom !== undefined ? this.zoom : settings.map.tiles.embeddedUrlMaxZoom,
+    zoom: this.zoom !== undefined ? this.zoom : settings.map.tiles.embeddedUrlMaxZoomWorld,
     maxZoom: settings.map.tiles.maxZoom,
     minZoom: 2,
     center: this.center || L.latLng(settings.map.unknownMapCenter as L.LatLngTuple),
