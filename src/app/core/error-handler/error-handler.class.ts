f3bcbc261dcc0580c0e1d7374dac14696156b8b4
ingljo <ingljo@gmail.com>
2018-11-30 14:17:19 +0100

    Try to avoid null errors

--- src/app/core/error-handler/error-handler.class.ts
+++ src/app/core/error-handler/error-handler.class.ts
@@ -3,11 +3,16 @@ import * as Sentry from 'sentry-cordova';
 
 export class AppErrorHandler extends ErrorHandler {
     handleError(error) {
-        super.handleError(error);
-        try {
-            Sentry.captureException(error.originalError || error);
-        } catch (e) {
-            console.error(e);
+        if (error) {
+            super.handleError(error);
+            try {
+                const err = error.originalError || error;
+                if (err) {
+                    Sentry.captureException(err);
+                }
+            } catch (e) {
+                console.error(e);
+            }
         }
     }
 }
