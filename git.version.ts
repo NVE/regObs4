fb705b8b54a3ca258ed063acb2f9dbb42c84c166
ingljo <ingljo@gmail.com>
2019-01-31 12:01:14 +0100

    Updated packages

--- git.version.ts
+++ git.version.ts
@@ -1,8 +1,9 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import { AppVersion } from './src/app/core/models/app-version.model';
 const cordovaSetVersion = require('cordova-set-version');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const prettifyXml = require('prettify-xml')
 
 async function getVersion(): Promise<AppVersion> {
   const revision = (await exec('git rev-parse --short HEAD')).stdout.toString().trim();
@@ -18,6 +19,12 @@ async function getVersion(): Promise<AppVersion> {
   };
 }
 
+function prettify(filePath: string) {
+  const options = { indent: 2, newline: '\n' };
+  const result = prettifyXml(readFileSync(filePath, { encoding: 'utf8' }), options);
+  writeFileSync(filePath, result, { encoding: 'utf8' });
+}
+
 async function updateVersion() {
   const version = await getVersion();
 
@@ -27,7 +34,9 @@ async function updateVersion() {
     branch: '${version.branch}'`);
 
   writeFileSync('src/environments/version.json', JSON.stringify(version), { encoding: 'utf8' });
-  cordovaSetVersion('./config.xml', version.version, version.buildNumber);
+  const configPath = 'config.xml';
+  cordovaSetVersion(configPath, version.version, version.buildNumber);
+  prettify(configPath);
 }
 
 updateVersion();
