495cb2dbf422d90d1657b2fe9cc810b013546076
ingljo <ingljo@gmail.com>
2019-02-13 13:10:56 +0100

    Fixed unit tests

--- src/app/modules/registration/components/numeric-input/numeric-input.component.spec.ts
+++ src/app/modules/registration/components/numeric-input/numeric-input.component.spec.ts
@@ -1,16 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NumericInputComponent } from './numeric-input.component';
+import { Platform } from '@ionic/angular';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { of, Observable } from 'rxjs';
 
 describe('NumericInputComponent', () => {
   let component: NumericInputComponent;
   let fixture: ComponentFixture<NumericInputComponent>;
+  let platformSpy: any;
+  let translateLoader: any;
+  let platformReadySpy: any;
+  let getTranslationSpy: (lang: string) => Observable<any>;
+  let isSpy: any;
+  let platform: any;
 
   beforeEach(async(() => {
+    platform = '';
+    platformReadySpy = Promise.resolve();
+    getTranslationSpy = (lang: string) => of({});
+    isSpy = (platformName: string) => platformName === platform;
+    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy, is: isSpy });
+    translateLoader = jasmine.createSpyObj('TranslateLoader', {
+      getTranslation: getTranslationSpy
+    });
     TestBed.configureTestingModule({
-      declarations: [ NumericInputComponent ]
-    })
-    .compileComponents();
+      declarations: [NumericInputComponent],
+      providers: [
+        { provide: Platform, useValue: platformSpy },
+      ],
+      imports: [
+        SharedModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useValue: translateLoader },
+        }),
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +47,11 @@ describe('NumericInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ios with decimals should give inputmode telephone', () => {
+    platform = 'ios';
+    component.decimalPlaces = 1;
+    expect(component.inputmode).toEqual('tel');
   });
 });
