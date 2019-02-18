2792194c2185ac9c2004d3969af302784a14aa93
ingljo <ingljo@gmail.com>
2019-02-18 15:19:24 +0100

    Save latest location in memory for faster loading

--- src/app/core/services/location/location.service.ts
+++ src/app/core/services/location/location.service.ts
@@ -6,20 +6,23 @@ import { AppMode } from '../../models/app-mode.enum';
 import { NanoSql } from '../../../../nanosql';
 import { ObsLocationsResponseDtoV2 } from '../../../modules/regobs-api/models';
 import { GeoHazard } from '../../models/geo-hazard.enum';
-import { switchMap, debounceTime, map, tap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import * as turf from '@turf/turf';
 import { LoginService } from '../../../modules/login/services/login.service';
 import { DbHelperService } from '../db-helper/db-helper.service';
-import { NanoSqlObservableHelper } from '../../helpers/nano-sql/nanoObserverToRxjs';
-import { BehaviorSubject, from, Observable, combineLatest } from 'rxjs';
+import { BehaviorSubject, combineLatest, of } from 'rxjs';
 import { LoggingService } from '../../../modules/shared/services/logging/logging.service';
+import { LogLevel } from '../../../modules/shared/services/logging/log-level.model';
+import { NanoSqlObservableHelper } from '../../helpers/nano-sql/nanoObserverToRxjs';
+
+const DEBUG_TAG = 'LocationService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
 
-  private changeTrigger: BehaviorSubject<void>;
+  private latestLocations: BehaviorSubject<{ [key: string]: ObsLocationsResponseDtoV2[] }>;
 
   constructor(
     private userSettingService: UserSettingService,
@@ -27,7 +30,7 @@ export class LocationService {
     private loggingService: LoggingService,
     private dbHelperService: DbHelperService,
     private apiLocationService: RegobsApi.LocationService) {
-    this.changeTrigger = new BehaviorSubject<void>(null);
+    this.latestLocations = new BehaviorSubject({});
   }
 
   async updateLocationWithinRadius(geoHazard: GeoHazard, lat: number, lng: number, radius: number) {
@@ -43,24 +46,48 @@ export class LocationService {
     };
     this.apiLocationService.rootUrl = settings.services.regObs.apiUrl[userSettings.appMode];
     const result = await this.apiLocationService.LocationWithinRadius(params).toPromise();
-    const tableName = NanoSql.getInstanceName(NanoSql.TABLES.LOCATION.name, userSettings.appMode);
-    await this.dbHelperService.fastInsert(tableName, result, (loc) => loc.Id, false);
-    // Cleanup deleted records. This also triggers change
-    await this.deleteLocationsNoLongerInResult(userSettings.appMode, geoHazard, lat, lng, radius, result);
 
-    // Trigger change
-    this.changeTrigger.next();
+    this.saveLatestLocations(result, userSettings.appMode, geoHazard);
+
+    this.saveResultsToDb(result, userSettings.appMode, geoHazard, lat, lng, radius);
+  }
+
+  private getLatestKey(appMode: AppMode, geoHazard: GeoHazard) {
+    return `${appMode}_${geoHazard}`;
+  }
+
+  private saveLatestLocations(items: ObsLocationsResponseDtoV2[], appMode: AppMode, geoHazard: GeoHazard) {
+    const currentValue = this.latestLocations.getValue();
+    currentValue[this.getLatestKey(appMode, geoHazard)] = items;
+    this.latestLocations.next(currentValue);
+  }
+
+  private async saveResultsToDb(items: ObsLocationsResponseDtoV2[],
+    appMode: AppMode, geoHazard: GeoHazard, lat: number, lng: number, radius: number) {
+    try {
+      const tableName = NanoSql.getInstanceName(NanoSql.TABLES.LOCATION.name, appMode);
+      await this.dbHelperService.fastInsert(tableName, items, (loc) => loc.Id, false);
+      // Cleanup deleted records. This also triggers change
+      await this.deleteLocationsNoLongerInResult(appMode, geoHazard, lat, lng, radius, items);
+    } catch (err) {
+      this.loggingService.log('Could not save new warnings to db', err, LogLevel.Warning, DEBUG_TAG);
+    }
   }
 
   getLocationsAsObservable(geoHazard: GeoHazard) {
-    return combineLatest(this.userSettingService.appMode$, this.changeTrigger)
-      .pipe(switchMap(([appMode, _]) =>
-        this.getLocationsFromDbAsObservable(
-          appMode,
-          geoHazard)),
-        debounceTime(200),
+    return combineLatest(this.userSettingService.appMode$, this.latestLocations.asObservable())
+      .pipe(switchMap(([appMode, latestLocations]) =>
+        this.getLatestLocationsOrFallbackToOffline(latestLocations, appMode, geoHazard)),
         tap((val) => this.loggingService.debug('Location observable changed', LocationService.name, val))
       );
+  }
+
+  private getLatestLocationsOrFallbackToOffline(latest: { [key: string]: ObsLocationsResponseDtoV2[] },
+    appMode: AppMode,
+    geoHazard: GeoHazard) {
+    return latest[this.getLatestKey(appMode, geoHazard)] !== undefined ?
+      of(latest[this.getLatestKey(appMode, geoHazard)])
+      : this.getLocationsFromDbAsObservable(appMode, geoHazard);
   }
 
   private async deleteLocationsNoLongerInResult(
@@ -75,17 +102,12 @@ export class LocationService {
     return turf.inside(turf.point([item.LatLngObject.Longitude, item.LatLngObject.Latitude]), buffered);
   }
 
-  getLocationsFromDbAsObservable(appMode: AppMode, geoHazard: GeoHazard): Observable<ObsLocationsResponseDtoV2[]> {
-    return from(
+  getLocationsFromDbAsObservable(appMode: AppMode, geoHazard: GeoHazard) {
+    return NanoSqlObservableHelper.toRxJS<ObsLocationsResponseDtoV2[]>(
       NanoSql.getInstance(NanoSql.TABLES.LOCATION.name, appMode)
-        .query('select').where((x) => x.GeoHazardId === geoHazard).exec()
+        .query('select').where(['GeoHazardId', '=', geoHazard]).listen({
+          debounce: 200
+        })
     );
   }
-
-  // getLocationsFromDbAsObservable(appMode: AppMode, geoHazard: GeoHazard) {
-  //   return NanoSqlObservableHelper.toRxJS<ObsLocationsResponseDtoV2[]>(
-  //     NanoSql.getInstance(NanoSql.TABLES.LOCATION.name, appMode)
-  //       .query('select').where(['GeoHazardId', '=', geoHazard]).listen()
-  //   );
-  // }
 }
