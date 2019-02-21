418a29f6064fea39f724c36533dc17689491710f
ingljo <ingljo@gmail.com>
2019-02-21 15:55:18 +0100

    Fixed map control icons

--- src/app/core/services/fullscreen/fullscreen.service.ts
+++ src/app/core/services/fullscreen/fullscreen.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,24 +10,25 @@ export class FullscreenService {
 
   _isFullscreen = false;
 
-  _subject: Subject<boolean>;
+  _subject: BehaviorSubject<boolean>;
 
   get isFullscreen$() {
     return this._subject.asObservable().pipe(shareReplay(1));
   }
 
   constructor(private statusBar: StatusBar) {
-    this._subject = new Subject<boolean>();
-    this._subject.next(this._isFullscreen);
+    this._subject = new BehaviorSubject(false);
+    this.isFullscreen$.subscribe((val) => {
+      this._isFullscreen = val;
+      if (val) {
+        this.statusBar.styleDefault();
+      } else {
+        this.statusBar.styleBlackTranslucent();
+      }
+    });
   }
 
   toggleFullscreen() {
-    this._isFullscreen = !this._isFullscreen;
-    this._subject.next(this._isFullscreen);
-    if (this._isFullscreen) {
-      this.statusBar.styleDefault();
-    } else {
-      this.statusBar.styleBlackTranslucent();
-    }
+    this._subject.next(!this._isFullscreen);
   }
 }
--- src/app/modules/map/components/map-controls/fullscreen-toggle/fullscreen-toggle.component.ts
+++ src/app/modules/map/components/map-controls/fullscreen-toggle/fullscreen-toggle.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FullscreenService } from '../../../../../core/services/fullscreen/fullscreen.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-fullscreen-toggle',
   templateUrl: './fullscreen-toggle.component.html',
   styleUrls: ['./fullscreen-toggle.component.scss']
 })
-export class FullscreenToggleComponent implements OnInit {
+export class FullscreenToggleComponent {
+
+  isFullscreen$: Observable<boolean>;
 
   constructor(private fullscreenService: FullscreenService) {
-
-  }
-
-  ngOnInit() {
+    this.isFullscreen$ = this.fullscreenService.isFullscreen$;
   }
 
   toggleFullscreen() {
     this.fullscreenService.toggleFullscreen();
+  }
+
+  getSrc(isFullscreen: boolean) {
+    return `/assets/icon/${isFullscreen ? 'collapse' : 'expand'}.svg`;
   }
 }
