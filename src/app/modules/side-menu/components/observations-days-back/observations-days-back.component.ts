2eb5fc1633f77376d7c1ddfe653aad05000340de
ingljo <ingljo@gmail.com>
2019-02-25 08:58:11 +0100

    rOa-364 - Observations dropdown component

--- src/app/modules/side-menu/components/observations-days-back/observations-days-back.component.ts
+++ src/app/modules/side-menu/components/observations-days-back/observations-days-back.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone, ViewChild } from '@angular/core';
 import { Subscription, combineLatest } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { IonSelect } from '@ionic/angular';
@@ -13,53 +13,51 @@ import { settings } from '../../../../../settings';
   styleUrls: ['./observations-days-back.component.scss']
 })
 export class ObservationsDaysBackComponent implements OnInit, OnDestroy {
-  daysBack: { val: number, selected: boolean, text: string }[];
+  selectedDaysBack: number;
+  daysBackOptions: { val: number, text: string }[];
   subscription: Subscription;
+
   constructor(private userSettingService: UserSettingService, private zone: NgZone, private loggingService: LoggingService) { }
 
   ngOnInit() {
     this.subscription = combineLatest(
       this.userSettingService.daysBack$,
       this.userSettingService.currentGeoHazardObservable$,
-      this.userSettingService.showObservations$,
-    )
-      .pipe(map(([daysBack, currentGeoHazard, showObservations]) => this.getDaysBackArray(daysBack, currentGeoHazard, showObservations)),
-        tap((val) =>
-          this.loggingService.debug(`Days back changed to ${val.find((d) => d.selected).val}`, 'ObservationsDaysBackComponent')))
-      .subscribe((val) => {
-        this.zone.run(() => {
-          this.daysBack = val;
-        });
+    ).subscribe(([daysBack, currentGeoHazard]) => {
+      const geoHazard = currentGeoHazard[0];
+      this.zone.run(() => {
+        this.daysBackOptions = this.getDaysBackArray(geoHazard);
+        const daysBackForCurrentGeoHazard = daysBack.find((x) => x.geoHazard === geoHazard);
+        if (daysBackForCurrentGeoHazard) {
+          this.selectedDaysBack = daysBackForCurrentGeoHazard.daysBack;
+        }
       });
+    });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  getDaysBackArray(daysBack: { geoHazard: GeoHazard, daysBack: number }[], currentGeoHazard: GeoHazard[], showObservations: boolean) {
-    const daysBackArr: { val: number, selected: boolean, text: string }[]
-      = settings.observations.daysBack[GeoHazard[currentGeoHazard[0]]].filter((x) => x > 0).map((val: number) => ({
-        val: val,
-        selected: daysBack.find((x) => x.geoHazard === currentGeoHazard[0]).daysBack === val,
-      }));
-    const today = {
-      val: 0,
-      selected: daysBack.find((x) => x.geoHazard === currentGeoHazard[0]).daysBack === 0,
-      text: 'MENU.TODAYS_OBSERVATIONS'
-    };
-    return [today, ...daysBackArr];
+  getDaysBackArray(geoHazard: GeoHazard) {
+    return settings.observations.daysBack[GeoHazard[geoHazard]].map((val: number) => ({
+      val: val,
+      text: val === 0 ? 'MENU.TODAYS_OBSERVATIONS' : undefined,
+    }));
   }
 
-  async changeDaysBack(event: Event) {
-    const select: IonSelect = (<any>event.target);
+  async save() {
     const userSetting = await this.userSettingService.getUserSettings();
+    let changed = false;
     for (const geoHazard of userSetting.currentGeoHazard) {
-      const existingValue = userSetting.observationDaysBack.find((x) => x.geoHazard === userSetting.currentGeoHazard[0]);
-      if (existingValue.daysBack !== select.value) {
-        existingValue.daysBack = select.value;
+      const existingValue = userSetting.observationDaysBack.find((x) => x.geoHazard === geoHazard);
+      if (existingValue.daysBack !== this.selectedDaysBack) {
+        existingValue.daysBack = this.selectedDaysBack;
+        changed = true;
       }
     }
-    await this.userSettingService.saveUserSettings(userSetting);
+    if (changed) {
+      await this.userSettingService.saveUserSettings(userSetting);
+    }
   }
 }
