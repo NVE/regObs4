7c36090b8adc8fd64d56acfa3ccc61848d50852a
ingljo <ingljo@gmail.com>
2019-03-06 10:22:59 +0100

    Another fix for change detection in gps center component

--- src/app/modules/map/components/map-controls/gps-center/gps-center.component.ts
+++ src/app/modules/map/components/map-controls/gps-center/gps-center.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { MapService } from '../../../services/map/map.service';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gps-center',
   templateUrl: './gps-center.component.html',
-  styleUrls: ['./gps-center.component.scss']
+  styleUrls: ['./gps-center.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GpsCenterComponent implements OnInit, OnDestroy {
+export class GpsCenterComponent implements OnInit, OnDestroy, AfterContentInit {
+
+
   followMode: boolean;
 
   private subscription: Subscription;
 
-  constructor(private mapService: MapService, private ngZone: NgZone) { }
+  constructor(private mapService: MapService, private cdRef: ChangeDetectorRef) { }
+
+  ngAfterContentInit(): void {
+    this.subscription = this.mapService.followMode$.subscribe((val) => {
+      this.followMode = val;
+      this.cdRef.detectChanges();
+    });
+    this.cdRef.detectChanges();
+  }
 
   ngOnInit() {
-    this.subscription = this.mapService.followMode$.subscribe((val) => {
-      this.ngZone.run(() => {
-        this.followMode = val;
-      });
-    });
   }
 
   ngOnDestroy(): void {
