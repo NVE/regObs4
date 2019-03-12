a513654a8fd30a1e3f9c2a8acfab4203e0faace4
ingljo <ingljo@gmail.com>
2019-03-12 09:26:34 +0100

    No need to load is in norway in web worker

--- src/app/core/helpers/leaflet/border-helper.ts
+++ src/app/core/helpers/leaflet/border-helper.ts
@@ -1,15 +1,11 @@
 import * as L from 'leaflet';
 import { settings } from '../../../../settings';
-import { Observable, Observer, of } from 'rxjs';
-import { createWorker } from 'typed-web-workers';
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { booleanContains, bboxPolygon, GeometryObject } from '@turf/turf';
-
-// export const NORWEGIAN_BORDER = require('../../../../assets/json/norway-borders.json');
 export const SVALBARD_BOUNDS =
     L.latLngBounds(settings.map.elevation.svalbard.bbox.map((coordinate) => L.latLng(coordinate[0], coordinate[1])));
-// export const NORWEGIAN_BOUNDS = L.geoJSON(NORWEGIAN_BORDER).getBounds();
 export const NORWEGIAN_BOUNDS = require('../../../../assets/json/world-topo-mix-border.json');
+export const NORWEGIAN_LATLNG_BOUNDS = L.geoJSON(NORWEGIAN_BOUNDS).getBounds();
 
 export class BorderHelper {
 
@@ -17,39 +13,14 @@ export class BorderHelper {
         return bounds.contains(latLng);
     }
 
+    static isInNorway(latLng: L.LatLng, bounds = NORWEGIAN_LATLNG_BOUNDS) {
+        return bounds.contains(latLng);
+    }
+
     static getLatLngBoundInSvalbardOrNorwayAsObservable(latLng: L.LatLng) {
-        return BorderHelper.isInSvalbard(latLng) ? of(({ inSvalbard: true, inNorway: true }))
-            : BorderHelper.isLatLngInNorwayAsObservable(latLng)
-                .pipe(map((inNorway) => ({ inSvalbard: false, inNorway })));
-    }
-
-    static isLatLngInNorwayAsObservable(latLng: L.LatLng, border = NORWEGIAN_BOUNDS): Observable<boolean> {
-        return Observable.create((observer: Observer<boolean>) => {
-            const typedWorker = createWorker(this.isLatLngInNorwayWorkFunc, (msg) => {
-                observer.next(msg);
-                observer.complete();
-            });
-            const input = {
-                baseUrl: document.location.protocol + '//' + document.location.host,
-                latLng: { lat: latLng.lat, lng: latLng.lng },
-                border: border.features[0].geometry
-            };
-            typedWorker.postMessage(input);
-            return () => typedWorker ? typedWorker.terminate() : null;
-        });
-    }
-
-    static isLatLngInNorwayWorkFunc(
-        input: {
-            baseUrl: string;
-            latLng: { lat: number, lng: number };
-            border: GeometryObject;
-        },
-        callback: (_: boolean) => void) {
-        const that = <any>self;
-        that.importScripts(`${input.baseUrl}/turf/turf.min.js`);
-        const result = that.turf.inside([input.latLng.lng, input.latLng.lat], input.border);
-        callback(result);
+        return this.isInSvalbard(latLng) ? of(({ inSvalbard: true, inNorway: true }))
+            : (this.isInNorway(latLng) ? of(({ inSvalbard: false, inNorway: true }))
+                : of(({ inSvalbard: false, inNorway: false })));
     }
 
     static toBBox(latLngBounds: L.LatLngBounds): [number, number, number, number] {
