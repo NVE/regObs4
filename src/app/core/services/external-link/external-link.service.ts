808a9ad0af15386c7f06cd54071808c5b5fc44eb
ingljo <ingljo@gmail.com>
2019-04-01 15:44:53 +0200

    Use ionic native for inAppBrowser. (It was the show() that caused tha Android crash)

--- src/app/core/services/external-link/external-link.service.ts
+++ src/app/core/services/external-link/external-link.service.ts
@@ -9,14 +9,6 @@ export class ExternalLinkService {
   constructor(private inAppBrowser: InAppBrowser) { }
 
   openExternalLink(url: string) {
-    // const iap = this.inAppBrowser.create(url, '_system');
-    // iap.show();
-    // NOTE: The above code crashes Android. Workaround is to use cordova plugin direct.
-    const w = (<any>window);
-    if (w.cordova && w.cordova.InAppBrowser) {
-      w.cordova.InAppBrowser.open(url, '_system', 'location=yes');
-    } else {
-      window.open(url, '_blank');
-    }
+    this.inAppBrowser.create(url, '_system', 'location=yes,hardwareback=yes');
   }
 }
