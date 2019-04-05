f82cef455afe3b2ffc45f8680050a8075f10bde9
ingljo <ingljo@gmail.com>
2019-04-05 14:30:44 +0200

    Try to improve performance on my observations

--- src/app/pages/my-observations/my-observations.page.ts
+++ src/app/pages/my-observations/my-observations.page.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit, ViewChild, OnDestroy, NgZone } from '@angular/core';
 import { ObservationService } from '../../core/services/observation/observation.service';
-import { Subscription, combineLatest, Observable, BehaviorSubject } from 'rxjs';
+import { Subscription, combineLatest, Observable } from 'rxjs';
 import { UserSettingService } from '../../core/services/user-setting/user-setting.service';
 import { LoginService } from '../../modules/login/services/login.service';
-import { IonInfiniteScroll, NavController, IonVirtualScroll } from '@ionic/angular';
+import { IonInfiniteScroll, NavController } from '@ionic/angular';
 import { ObserverResponseDto, RegistrationViewModel } from '../../modules/regobs-api/models';
 import { RegistrationService } from '../../modules/registration/services/registration.service';
-import * as moment from 'moment';
 import { map, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { IRegistration } from '../../modules/registration/models/registration.model';
-// import { ObsCardHeightService } from '../../core/services/obs-card-height/obs-card-height.service';
 
 interface MyVirtualScrollItem {
   type: 'draft' | 'sync' | 'sent';
@@ -23,6 +21,8 @@ const LIST_HEADERS = {
   'sent': { header: 'MY_OBSERVATIONS.MY_SENT_OBSERVATIONS', subtitle: 'MY_OBSERVATIONS.SENT_SUBTITLE' },
 };
 
+const itemsToLoad = 20;
+
 @Component({
   selector: 'app-my-observations',
   templateUrl: './my-observations.page.html',
@@ -30,15 +30,12 @@ const LIST_HEADERS = {
 })
 export class MyObservationsPage implements OnInit, OnDestroy {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-  @ViewChild(IonVirtualScroll) virtualScroll: IonVirtualScroll;
   private registrationSubscription: Subscription;
   private user: ObserverResponseDto;
-  // theBoundCallback: Function;
   loaded = false;
   refreshFunc = this.refresh.bind(this);
   private firstDataLoad = false;
   virtualItems: MyVirtualScrollItem[] = [];
-  myObservations: BehaviorSubject<RegistrationViewModel[]>;
 
   get showEmptyState() {
     return this.loaded && this.virtualItems.length === 0 && this.firstDataLoad;
@@ -50,155 +47,37 @@ export class MyObservationsPage implements OnInit, OnDestroy {
     private userSettingService: UserSettingService,
     private navContoller: NavController,
     private registrationService: RegistrationService,
-    // private obsCardHeightService: ObsCardHeightService,
     private loginService: LoginService) {
-    this.myObservations = new BehaviorSubject([]);
   }
 
-  refresh(cancelPromise: Promise<any>) {
-    return this.registrationService.syncRegistrations(cancelPromise).then(() =>
-      this.loadData(cancelPromise, true));
+  async refresh(cancelPromise: Promise<any>) {
+    await this.registrationService.syncRegistrations(cancelPromise);
+    this.initRegistrationSubscription();
   }
 
   async ngOnInit() {
-    // this.theBoundCallback = this.getItemHeight.bind(this);
     const loggedInUser = await this.loginService.getLoggedInUser();
     if (!loggedInUser.isLoggedIn) {
       this.navContoller.navigateRoot('/');
     } else {
       this.user = loggedInUser.user;
     }
+    this.initRegistrationSubscription();
+  }
 
-    this.registrationSubscription = this.getVirtualScrollItemsObservable().subscribe((val) => {
-      this.updateVirtualItems(val);
-    });
-
-    this.loadData(null, true);
+  private initRegistrationSubscription() {
+    this.registrationSubscription = this.getVirtualScrollItemsObservable().subscribe((val) => this.updateVirtualItems(val));
   }
 
   private updateVirtualItems(virtualItems: MyVirtualScrollItem[]) {
-    if (!this.loaded) {
-      this.initVirtualItems(virtualItems);
-    } else {
+    setTimeout(() => {
       this.ngZone.run(() => {
-        const reloadMyObservations = this.virtualItems.filter((x) => x.type === 'sync').length !==
-          virtualItems.filter((x) => x.type === 'sync').length;
-
-        const indexUpdates = this.mergeVirtualItems(virtualItems);
-        const indexRange = this.getIndexRange(indexUpdates);
-        setTimeout(() => {
-          this.checkRange(indexRange.min, indexRange.max);
-        });
-
-        if (reloadMyObservations) {
-          this.myObservations.next([]);
-          this.loadData(null, true);
-        }
+        this.loaded = true;
       });
-    }
-  }
-
-  private getIndexRange(indexesToCheck: number[]) {
-    if (indexesToCheck && indexesToCheck.length > 0) {
-      return { min: Math.min(...indexesToCheck), max: Math.max(...indexesToCheck) };
-    } else {
-      return { min: 0, max: 0 };
-    }
-  }
-
-  private checkRange(index: number, toIndex: number) {
-    if (this.virtualScroll) {
-      this.virtualScroll.checkRange(index); // checking all items after this index
-    }
-  }
-
-  // TODO: Implement custom List IterableDiffer
-  // https://blog.mgechev.com/2017/11/14/angular-iterablediffer-keyvaluediffer-custom-differ-track-by-fn-performance/
-
-  private mergeVirtualItems(virtualItems: MyVirtualScrollItem[]) {
-    const checkIndexes = [];
-    for (const existingItem of this.virtualItems) {
-      if (!virtualItems.find((x) => x.type === existingItem.type && x.id === existingItem.id)) {
-        // Item has been removed
-        const index = this.virtualItems.indexOf(existingItem);
-        this.virtualItems.splice(index, 1);
-        checkIndexes.push(index);
-      }
-    }
-
-    for (const virtualItem of virtualItems) {
-      const findItemIndex = this.findItemIndex(virtualItem);
-      if (findItemIndex.changed) {
-        this.virtualItems.splice(findItemIndex.index, findItemIndex.replace, virtualItem);
-        checkIndexes.push(findItemIndex.index);
-      }
-    }
-    return checkIndexes;
-  }
-
-  private findItemIndex(item: MyVirtualScrollItem) {
-    const existingIndex = this.virtualItems.findIndex((x) => x.type === item.type && x.id === item.id);
-    if (existingIndex >= 0) {
-      const changed = this.hasChanged(item, this.virtualItems[existingIndex]);
-      return { index: existingIndex, replace: changed ? 1 : 0, changed };
-    } else {
-      const i = this.virtualItems.findIndex((x) => this.isBefore(item, x));
-      if (i >= 0) {
-        return { index: i, replace: 0, changed: true };
-      } else {
-        return { index: this.virtualItems.length, replace: 0, changed: true };
-      }
-    }
-  }
-
-  private hasChanged(item: MyVirtualScrollItem, currentItem: MyVirtualScrollItem) {
-    if (item.type === 'sent' && currentItem.type === 'sent') {
-      return !(moment((<RegistrationViewModel>item.item).DtChangeTime)
-        .isSame(moment((<RegistrationViewModel>currentItem.item).DtChangeTime)));
-    }
-    return false;
-  }
-
-  private isBefore(item: MyVirtualScrollItem, currentItem: MyVirtualScrollItem) {
-    if (item.type === 'draft' || item.type === 'sync') {
-      return this.isDraftBefore(item, currentItem);
-    } else {
-      return this.isRegistrationBefore(item, currentItem);
-    }
-  }
-
-  private isDraftBefore(item: MyVirtualScrollItem, currentItem: MyVirtualScrollItem) {
-    if (currentItem.type === 'sent' || (item.type === 'sync' && currentItem.type === 'draft')) {
-      return true;
-    } else if (currentItem.type === item.type) {
-      return item.id < currentItem.id;
-    } else {
-      return false;
-    }
-  }
-
-  private isRegistrationBefore(item: MyVirtualScrollItem, currentItem: MyVirtualScrollItem) {
-    if (currentItem.type === 'draft' || currentItem.type === 'sync') {
-      return false;
-    } else if (item.type === 'sent' && currentItem.type === 'sent') {
-      return moment((<RegistrationViewModel>item.item).DtObsTime).isAfter(moment((<RegistrationViewModel>currentItem.item).DtObsTime));
-    } else {
-      return false;
-    }
-  }
-
-  private initVirtualItems(virtualItems: MyVirtualScrollItem[]) {
+    }, virtualItems.length > 0 ? 2000 : 0);
     this.ngZone.run(() => {
       this.virtualItems = virtualItems;
     });
-    setTimeout(() => {
-      this.virtualItems = [...virtualItems];
-      setTimeout(() => {
-        this.ngZone.run(() => {
-          this.loaded = true;
-        });
-      }, 500);
-    }, 500);
   }
 
   myHeaderFn(record: MyVirtualScrollItem, index: number, items: MyVirtualScrollItem[]) {
@@ -216,16 +95,17 @@ export class MyObservationsPage implements OnInit, OnDestroy {
   private getVirtualScrollItemsObservable() {
     return combineLatest(
       this.getSyncItemsObservable(),
-      this.getDraftObservable(),
-      this.getSentObservable())
-      .pipe(map(([a, b, c]) => ([...a, ...b, ...c])));
+      this.getDraftObservable())
+      .pipe(map(([a, b]) => ([...a, ...b])),
+        switchMap((syncItems) => this.getMyRegistrationsObservable().pipe(
+          map((val) => ([...syncItems, ...val])))));
   }
 
-  private getSentObservable(): Observable<MyVirtualScrollItem[]> {
-    return this.myObservations.asObservable().pipe(
-      distinctUntilChanged<RegistrationViewModel[], string>
-        ((a, b) => a.localeCompare(b) === 0, (keySelector) => this.getDistinctRegistrationList(keySelector)),
-      map((val) => val.map((item) => ({ type: <'sent'>'sent', id: item.RegID.toString(), item }))));
+  private getMyRegistrationsObservable() {
+    return this.userSettingService.appModeAndLanguage$.pipe(switchMap(([appMode, langKey]) =>
+      this.observationService.getObservationsForCurrentUser(appMode, this.user, langKey, 0, itemsToLoad).pipe(
+        map((val) => val.map((item) => ({ type: <'sent'>'sent', id: item.RegID.toString(), item }))))
+    ));
   }
 
   private getDraftObservable(): Observable<MyVirtualScrollItem[]> {
@@ -254,17 +134,15 @@ export class MyObservationsPage implements OnInit, OnDestroy {
     }
   }
 
-  async loadData(cancel?: Promise<any>, forceReload = false) {
-    const currentValue = forceReload ? [] : this.myObservations.value;
-    const numberOfRecords = 10;
-    const pageNumber = Math.floor(currentValue.length / numberOfRecords);
+  async loadMoreData() {
+    const currentLength = this.virtualItems.filter((x) => x.type === 'sent').length;
+    const pageNumber = Math.floor(currentLength / itemsToLoad);
     const subscription = this.userSettingService.appModeAndLanguage$.pipe(switchMap(([appMode, langKey]) =>
-      this.observationService.getObservationsForCurrentUser(appMode, this.user, langKey, pageNumber, numberOfRecords)
+      this.observationService.getObservationsForCurrentUser(appMode, this.user, langKey, pageNumber, itemsToLoad)
     )).subscribe((val) => {
-      currentValue.push(...val);
-      this.myObservations.next(currentValue);
+      const sentItems = val.map((item) => ({ type: <'sent'>'sent', id: item.RegID.toString(), item }));
       this.ngZone.run(() => {
-        this.firstDataLoad = true;
+        this.virtualItems.push(...sentItems);
         this.infiniteScroll.complete();
       });
     }, (_) => {
@@ -272,14 +150,10 @@ export class MyObservationsPage implements OnInit, OnDestroy {
         this.infiniteScroll.complete();
       });
     });
-    if (cancel) {
-      cancel.then(() => {
-        subscription.unsubscribe();
-      });
-    }
   }
-  // trackByRegId(index: number, obs: RegistrationViewModel) {
-  //   return obs ? obs.RegID : undefined;
-  // }
+
+  trackById(index: number, item: MyVirtualScrollItem) {
+    return item ? `${item.type}_${item.id}` : undefined;
+  }
 
 }
