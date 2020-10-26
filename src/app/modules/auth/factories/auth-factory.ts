df5f4778eda60d9655853b4b01b64f773b3a0980
ingljo <ingljo@gmail.com>
2020-10-26 21:21:36 +0100

    Fixed redirect to login when password changed

--- src/app/modules/auth/factories/auth-factory.ts
+++ src/app/modules/auth/factories/auth-factory.ts
@@ -1,6 +1,6 @@
 import { Platform } from '@ionic/angular';
 import { StorageBackend, Requestor } from '@openid/appauth';
-import { AuthService, Browser, ConsoleLogObserver } from 'ionic-appauth';
+import { AuthService, Browser } from 'ionic-appauth';
 import { NgZone } from '@angular/core';
 import { UserSettingService } from '../../../core/services/user-setting/user-setting.service';
 import { settings } from '../../../../settings';
@@ -10,12 +10,11 @@ export let authFactory = (platform: Platform, ngZone: NgZone,
   requestor: Requestor, browser: Browser, storage: StorageBackend, userSettingService: UserSettingService) => {
 
   const authService = new AuthService(browser, storage, requestor);
-
   userSettingService.appMode$.subscribe((appMode: AppMode) => {
     authService.authConfig = settings.authConfig[appMode];
     if (!platform.is('cordova')) {
       authService.authConfig.redirect_url = window.location.origin + '/auth/callback';
-      authService.authConfig.end_session_redirect_url = window.location.origin + '/auth/endsession';
+      authService.authConfig.end_session_redirect_url = window.location.origin + '/auth/callback';
     }
   });
   return authService;
