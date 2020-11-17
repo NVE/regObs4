5803e785dd4d17d846815f63eb6b817e6ae39c7e
ingljo <ingljo@gmail.com>
2020-11-17 15:32:12 +0100

    Fix sentry bug https://sentry.io/share/issue/38b414e24f554991b526b1bbdda120f9/

--- src/app/core/helpers/async-filter.ts
+++ src/app/core/helpers/async-filter.ts
@@ -1,5 +1,8 @@
 export async function asyncFilter<T>(arr: T[], callback: (item: T) => Promise<boolean>): Promise<T[]> {
   const fail = Symbol();
+  if(!arr || !Array.isArray(arr)) {
+    return Promise.resolve([]);
+  }
   return (await Promise.all(arr.map(async item => (await callback(item)) ? item : fail)))
     .filter(i => i !== fail) as T[];
 }
