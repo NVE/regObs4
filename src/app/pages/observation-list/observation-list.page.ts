47074abd607c0891a31e6673b571a94f5737e2a7
omy <omy@nve.no>
2021-01-22 13:11:14 +0100

    Forenklet listevisning. Nå henter vi maks 100 observasjoner. Mangler melding til bruker når man har scrollet til bunn om at det ikke er flere. Ikke testet på telefon ennå

--- src/app/pages/observation-list/observation-list.page.ts
+++ src/app/pages/observation-list/observation-list.page.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ObservationService } from '../../core/services/observation/observation.service';
 import * as L from 'leaflet';
-import { Observable, Subject } from 'rxjs';
-import { map, take, switchMap, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { map, take, switchMap } from 'rxjs/operators';
 import { MapService } from '../../modules/map/services/map/map.service';
 import { IMapView } from '../../modules/map/services/map/map-view.interface';
 import { RegistrationViewModel } from '../../modules/regobs-api/models';
 import { IonContent, IonInfiniteScroll } from '@ionic/angular';
 import { LoggingService } from '../../modules/shared/services/logging/logging.service';
 import { DataMarshallService } from '../../core/services/data-marshall/data-marshall.service';
-import { LogLevel } from '../../modules/shared/services/logging/log-level.model';
-// import { VirtualScrollerComponent } from 'ngx-virtual-scroller';
 
-const DEBUG_TAG = 'ObservationListPage';
 const PAGE_SIZE = 10;
+const MAX_OBSERVATION_COUNT = 100;
 
 @Component({
   selector: 'app-observation-list',
@@ -22,28 +20,18 @@ const PAGE_SIZE = 10;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ObservationListPage implements OnInit {
-  observations: RegistrationViewModel[];
+  visibleObservations: RegistrationViewModel[];
+  allObservations: RegistrationViewModel[];
   loaded = false;
   cancelSubject: Subject<unknown>;
   private pageIndex = 0;
   private total: number;
-  // parentScrollElement: HTMLElement;
 
   @ViewChild(IonContent, { static: true }) content: IonContent;
-  // @ViewChild(VirtualScrollerComponent, { static: false }) scroll: VirtualScrollerComponent;
+  @ViewChild(IonInfiniteScroll, { static: false}) scroll: IonInfiniteScroll;
 
   trackByIdFunc = this.trackByIdFuncInternal.bind(this);
   refreshFunc = this.refresh.bind(this);
-
-  get observations$(): Observable<RegistrationViewModel[]> {
-    return this.mapService.mapView$.pipe(switchMap((mapView: IMapView) =>
-      this.observationService.observations$.pipe(
-        map((observations) => this.filterObservationsWithinViewBounds(observations, mapView)),
-        tap((observations) => this.total = observations.length),
-        map((observations) => observations.slice(this.pageIndex * PAGE_SIZE, PAGE_SIZE)))),
-      take(1),
-    );
-  }
 
   constructor(
     private observationService: ObservationService,
@@ -55,7 +43,6 @@ export class ObservationListPage implements OnInit {
 
   ngOnInit(): void {
     this.cancelSubject = this.dataMarshallService.observableCancelSubject;
-    // this.parentScrollElement = await this.content.getScrollElement();
   }
 
   refresh(cancelPromise: Promise<unknown>): void {
@@ -69,12 +56,12 @@ export class ObservationListPage implements OnInit {
 
   ionViewWillLeave(): void {
     this.loaded = false;
-    this.observations = undefined;
+    this.visibleObservations = undefined;
   }
 
   private async resetAndLoadObservations(forceUpdate = false, cancelPromise: Promise<unknown> = undefined): Promise<void> {
     this.loaded = false;
-    this.observations = undefined;
+    this.visibleObservations = undefined;
     this.cdr.detectChanges();
     if (forceUpdate) {
       await this.observationService.forceUpdateObservationsForCurrentGeoHazard(cancelPromise);
@@ -82,33 +69,40 @@ export class ObservationListPage implements OnInit {
     this.loadObservations();
   }
 
-  private loadObservations() {
-    this.observations$.subscribe((observations) => {
-      this.observations = observations;
+  private async loadObservations() {
+    this.allObservations = await this.getObservationsInMap();
+    this.total = this.allObservations.length;
+    this.pageIndex = 0;
+    this.visibleObservations = this.allObservations.slice(0, 10);
+
+    this.cdr.detectChanges();
+    setTimeout(() => {
+      this.loaded = true;
+      this.scroll.disabled = false;
       this.cdr.detectChanges();
-      setTimeout(() => {
-        this.loaded = true;
-        this.cdr.detectChanges();
-      }, 500);
-    }, (err) => {
-      this.loggingService.log('Could not load observations', err, LogLevel.Warning, DEBUG_TAG);
-    });
+    }, 500);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private getObservationsInMap(): Promise<RegistrationViewModel[]> {
+    return this.mapService.mapView$.pipe(switchMap((mapView: IMapView) =>
+      this.observationService.observations$.pipe(
+        map((observations) => this.filterObservationsWithinViewBounds(observations, mapView)),
+        map((observations) => observations.slice(0, MAX_OBSERVATION_COUNT))
+      )),
+    take(1)
+    ).toPromise();
+  }
+
   loadNextPage(event: CustomEvent<IonInfiniteScroll>): void {
-    this.observations$.subscribe((observations) => {
-      this.observations.push(...observations);
-      const target: IonInfiniteScroll = event.target as unknown as IonInfiniteScroll;
-      target.complete();
-      if (this.observations.length >= this.total) {
-        target.disabled = true;
-      }
-    });
-  }
+    this.pageIndex += 1;
+    const startIndex = this.pageIndex * PAGE_SIZE;
+    this.visibleObservations.push(...this.allObservations.slice(startIndex, startIndex + PAGE_SIZE));
 
-  compareItems(a: RegistrationViewModel, b: RegistrationViewModel): boolean {
-    return a?.RegID === b?.RegID;
+    const target: IonInfiniteScroll = event.target as unknown as IonInfiniteScroll;
+    target.complete();
+    if (this.visibleObservations.length >= this.total) {
+      target.disabled = true; //we have reached the end, so no need to load more pages from now
+    }
   }
 
   private filterObservationsWithinViewBounds(observations: RegistrationViewModel[], view: IMapView) {
