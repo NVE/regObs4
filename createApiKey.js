a63aa399f405e0636f4d92009ced2a686469d9c1
Jørgen Loe Kvalberg <jorgen.kvalberg@gmail.com>
2021-02-19 10:57:06 +0100

    Fix apikey.json filename

--- createApiKey.js
+++ createApiKey.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 
 const apiKey = process.env.REGOBS_API_KEY;
 const data = JSON.stringify({ apiKey });
-fs.writeFileSync("./src/assets/apiKey.json", data);
+fs.writeFileSync("./src/assets/apikey.json", data);
