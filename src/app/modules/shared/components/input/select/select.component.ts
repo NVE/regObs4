f222959563d2b86ba3ed66db5846aff543cfaf26
Nicolai Michelet <nicomi@nve.no>
2021-03-29 15:47:38 +0200

    possible fix select onChange problems

--- src/app/modules/shared/components/input/select/select.component.ts
+++ src/app/modules/shared/components/input/select/select.component.ts
@@ -120,4 +120,9 @@ export class SelectComponent implements OnInit {
     this.selectedValue = id;
     this.selectedValueChange.emit(id);
   }
+
+  onChange(event) {
+    this.selectedValue = event.target.value;
+    this.selectedValueChange.emit(this.selectedValue);
+  }
 }
