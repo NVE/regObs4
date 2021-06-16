be376bd218453a9cc8958ded4ffc9b99e969d574
Jørgen Loe Kvalberg <jorgen.kvalberg@gmail.com>
2021-06-16 15:02:44 +0200

    Add license info

--- src/app/modules/shared/services/logging/file-logging.service.ts
+++ src/app/modules/shared/services/logging/file-logging.service.ts
@@ -1,3 +1,32 @@
+/**
+ * Based on https://github.com/SmartMoveSystems/ionicLogFileAppender
+ * SmartMove Ionic rolling log file appender
+ * CellTrack Systems Pty Ltd 2018
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 SmartMove Systems
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 import {formatDate} from '@angular/common';
 import {Entry, File} from '@ionic-native/file/ngx';
 import {Injectable} from '@angular/core';
@@ -8,11 +37,6 @@ import { EmailComposer, EmailComposerOptions } from '@ionic-native/email-compose
 import { settings } from 'src/settings';
 import { LogLevel } from './log-level.model';
 
-/**
- * Based on https://github.com/SmartMoveSystems/ionicLogFileAppender
- * SmartMove Ionic rolling log file appender
- * CellTrack Systems Pty Ltd 2018
- */
  @Injectable({
   providedIn: 'root'
 })
