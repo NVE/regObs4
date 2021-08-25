14efb21cbac36139cfe9e3859354c097531364b0
Nicolai Michelet <nicomi@nve.no>
2021-08-25 16:49:15 +0200

    only show on larger screens, text formatting

--- src/app/modules/side-menu/components/show-filter-criteria/show-filter-criteria.component.ts
+++ src/app/modules/side-menu/components/show-filter-criteria/show-filter-criteria.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { BreakpointService } from '../../../../core/services/breakpoint.service';
 import { UserSettingService } from '../../../../core/services/user-setting/user-setting.service';
 
 @Component({
@@ -11,13 +12,17 @@ import { UserSettingService } from '../../../../core/services/user-setting/user-
 export class ShowFilterCriteriaComponent implements OnInit {
 
   daysBack$: Observable<{value: number}>;
+  isDesktop: boolean;
 
-  constructor(public userSettingService: UserSettingService) { }
+  constructor(public userSettingService: UserSettingService, private breakpointService: BreakpointService) { }
 
   ngOnInit() {
-    this.daysBack$ = this.userSettingService.daysBackForCurrentGeoHazard$.
-      pipe(map( value => ({
+    this.daysBack$ = this.userSettingService.daysBackForCurrentGeoHazard$.pipe(
+      map((value) => ({
         value
       })));
+    this.breakpointService.isDesktopView().subscribe((isDesktop) => {
+      this.isDesktop = isDesktop;
+    });
   }
 }
