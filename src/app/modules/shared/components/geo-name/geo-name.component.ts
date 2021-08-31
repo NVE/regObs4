3911fdd7fd0116ba98c2f3fa4cd6a2a4549952ea
Nicolai Michelet <nicomi@nve.no>
2021-08-31 15:55:10 +0200

    fix failed test

--- src/app/modules/shared/components/geo-name/geo-name.component.ts
+++ src/app/modules/shared/components/geo-name/geo-name.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnChanges } from '@angular/core';
-import { GeoHazard } from 'src/app/modules/common-core/models';
-import { Observable } from 'rxjs';
+import { GeoHazard, LangKey } from 'src/app/modules/common-core/models';
+import { Observable, switchMap } from 'rxjs';
 import { GeoHelperService } from '../../services/geo-helper/geo-helper.service';
+import { UserSettingService } from 'src/app/core/services/user-setting/user-setting.service';
 
 @Component({
   selector: 'app-geo-name',
@@ -12,10 +13,14 @@ export class GeoNameComponent implements OnChanges {
   @Input() geoHazards: GeoHazard[];
 
   name$: Observable<string>;
+  language$: Observable<LangKey>;
 
-  constructor(private geoHelperService: GeoHelperService) {}
+  constructor(private geoHelperService: GeoHelperService, private userSettingsService: UserSettingService) {
+  }
 
   ngOnChanges(): void {
-    this.name$ = this.geoHelperService.getName(this.geoHazards);
+    this.name$ = this.userSettingsService.language$.pipe(
+      switchMap(() => this.geoHelperService.getName(this.geoHazards))
+    );
   }
 }
