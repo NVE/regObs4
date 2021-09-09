9b8b51dd7b553e346453531f95c714028a663037
ingljo <ingljo@gmail.com>
2021-09-09 13:56:29 +0200

    Try to improve timing beforeEach in tests

--- src/app/core/services/offline-map/offline-map.service.spec.ts
+++ src/app/core/services/offline-map/offline-map.service.spec.ts
@@ -1,24 +1,31 @@
+import { Platform } from '@ionic/angular';
+import { Platforms } from '@ionic/core';
 import { of } from 'rxjs';
 import { TestLoggingService } from '../../../modules/shared/services/logging/test-logging.service';
-import { CompoundPackage, CompoundPackageMetadata } from '../../../pages/offline-map/metadata.model';
+import { CompoundPackage } from '../../../pages/offline-map/metadata.model';
 import { OfflineMapService } from './offline-map.service';
 import { ProgressStep } from './progress-step.model';
 
 describe('OfflineMapService', () => {
   let offlineMapService: OfflineMapService;
+  let platformMock: Platform;
 
-  it('progress value for 10% for download step of part 1 of 2 should be 0.10 / 4 =  0.025', () => {
-    offlineMapService = new OfflineMapService(null, new TestLoggingService(), null, null, null, null, null, null);
+  beforeEach(() => {
+    platformMock = jasmine.createSpyObj('Platform', {
+      is: (platformName: Platforms) => false
+    });
+    offlineMapService = new OfflineMapService(null, new TestLoggingService(), null, null, platformMock, null, null, null);
+  });
+
+  it('progress value for 10% for download step of part 1 of 2 should be 0.10 / 4 =  0.025', () => {  
     expect(offlineMapService.calculateTotalProgress(0.10, 0, 2, 'Downloading')).toBe(0.025);
   });
 
   it('progress value for 10% for unzip step of part 2 of 2 should be (0.10 / 4) + (3/4)  =  0.025 + 0.75 =  0.775', () => {
-    offlineMapService = new OfflineMapService(null, new TestLoggingService(), null, null, null, null, null, null);
     expect(offlineMapService.calculateTotalProgress(0.10, 1, 2, 'Unzipping')).toBe(0.775);
   });
 
   it('get needed diskspace adds compression factor', async () => {
-    offlineMapService = new OfflineMapService(null, new TestLoggingService(), null, null, null, null, null, null);
     const cp = new CompoundPackage({
       bbox: [1, 2, 3, 4],
       id: 'test',
@@ -34,7 +41,6 @@ describe('OfflineMapService', () => {
   });
 
   it('get needed diskspace also sums up items in queue and currently downloading', async () => {
-    offlineMapService = new OfflineMapService(null, new TestLoggingService(), null, null, null, null, null, null);
     const cp = new CompoundPackage({
       bbox: [1, 2, 3, 4],
       id: 'test',
