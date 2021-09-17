28ca48ea0694c8b285dbd74228093f1cabea4ab7
omy <omy@nve.no>
2021-09-17 17:37:23 +0200

    Nå oppfrisker vi token hvis vi får 401 fra API'et og prøver kallet på nytt

--- src/app/core/http-interceptor/ApiInterceptor.ts
+++ src/app/core/http-interceptor/ApiInterceptor.ts
@@ -1,4 +1,4 @@
-import { catchError, switchMap, tap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import {
   HttpRequest,
   HttpInterceptor,
@@ -11,6 +11,10 @@ import { settings } from '../../../settings';
 import { LoggingService } from 'src/app/modules/shared/services/logging/logging.service';
 import { RegobsAuthService } from 'src/app/modules/auth/services/regobs-auth.service';
 
+/**
+ * Sender innloggings-token med kall til Regobs API der kallene krever at man er logget inn.
+ * Hvis api-kallet feiler pga. innloggingsfeil (HTTP 401), prøver vi å fornye tokenet og kjører kallet en gang til
+ */
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
   constructor(private regobsAuthService: RegobsAuthService, private loggerService: LoggingService) {
@@ -31,24 +35,40 @@ export class ApiInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // Apply the headers
     if (this.isRegObsApi(req.url) && !req.headers.has('Authorization')) {
-      return from(this.regobsAuthService.getValidToken()).pipe(catchError((err) => {
-        // TODO: Route to login
-        this.loggerService.debug('Could not get valid token', 'API interceptor', err);
-        this.regobsAuthService.signIn();
-        return EMPTY;
-      }), switchMap((result) => {
-          const headers = req.headers.set(
-            'Authorization',
-            `Bearer ${result.idToken}`
-          );
-
-          const requestClone = req.clone({ headers });
-          return next.handle(requestClone);
-      }));
+      return this.addAuthHeader(req).pipe(
+        switchMap((requestWithAuthHeader) => {
+          return next.handle(requestWithAuthHeader).pipe(catchError(err => {
+            return this.handleResponseError(err, requestWithAuthHeader, next);
+          }));
+        }));
     }
-
     return next.handle(req);
+  }
+
+  private addAuthHeader(request: HttpRequest<any>): Observable<HttpRequest<any>> {
+    return this.regobsAuthService.loggedInUser$.pipe(catchError((err) => {
+      this.loggerService.debug('Could not get valid token', 'API interceptor', err);
+      this.regobsAuthService.signIn();
+      return EMPTY; //TODO: Why this?
+    }), map((user) => {
+        const headers = request.headers.set(
+          'Authorization',
+          `Bearer ${user.token}`
+        );
+        return request.clone({ headers });
+    }));
+  }
+
+  private handleResponseError(error, request, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (error.status === 401) {
+      // Vi er ikke autorisert, trolig fordi tokenet ikke er gyldig
+      this.loggerService.debug('Got 401 from API, trying to refresh token and repeat API-call...');
+      return from(this.regobsAuthService.refreshToken()).pipe(
+        switchMap(() => this.addAuthHeader(request)),
+        switchMap((req) => next.handle(req))
+      )
+    }
+    throw (error);
   }
 }
