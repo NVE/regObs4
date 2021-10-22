2dc83f31d248dfa52d415d73f1f78a5793b659f9
gruble <omy@nve.no>
2021-10-22 11:07:01 +0200

    la inn Arons elegante snutt for å konvertere byte array til Base64

--- src/app/core/services/offline-map/offline-map.service.ts
+++ src/app/core/services/offline-map/offline-map.service.ts
@@ -28,11 +28,7 @@ const ROOT_MAP_DIR = 'maps';
 
 function arrayBufferToBase64(buffer: ArrayBuffer) {
   let binary = '';
-  const bytes = new Uint8Array(buffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
+  new Uint8Array(buffer).forEach((byte) => binary += String.fromCharCode(byte));
   return btoa(binary);
 }
 
