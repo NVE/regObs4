0a06cb9ba59e6c4f632df1351f743209d2250cc1
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2021-12-01 16:40:46 +0100

    RO 1509: Vise kartsenter-koordinater og avstand uten forsinkelse (#124)
    Nå beregner vi og viser horisontal avstand til kartsenter uavhengig av api-kallet vi gjør for å slå opp høyde og kommune + region
    - Fant en løsning på at man ikke får kartsenter-koordinater når man starter appen
    - Nå viser vi kartsenter-koordinater også før vi har panorert. 
    - Fant også en enklere måte å beregne høyde på den svarte boksen, og visning og plassering av komponenten er flyttet til HomeComponent
    
    Co-authored-by: Aron Widforss <aron@antarkt.is>
    Co-authored-by: Jørgen Loe Kvalberg <jorgkv@users.noreply.github.com>

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -26,6 +26,7 @@ import { CompetenceComponent } from '../../components/competence/competence.comp
 import { MapImageModule } from '../map-image/map-image.module';
 import { AuthModule } from './../auth/auth.module';
 import { VirtualScrollerModule } from 'ngx-virtual-scroller';
+import { AbsPipe } from './pipes/abs.pipe';
 @NgModule({
   imports: [
     CommonModule,
@@ -57,7 +58,8 @@ import { VirtualScrollerModule } from 'ngx-virtual-scroller';
     SelectComponent,
     GeoFabComponent,
     GeoNameComponent,
-    CompetenceComponent
+    CompetenceComponent,
+    AbsPipe
   ],
   exports: [
     CommonModule,
@@ -86,7 +88,8 @@ import { VirtualScrollerModule } from 'ngx-virtual-scroller';
     GeoFabComponent,
     CompetenceComponent,
     AuthModule,
-    VirtualScrollerModule
+    VirtualScrollerModule,
+    AbsPipe
   ]
 })
 export class SharedModule {}
