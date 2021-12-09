f8568ebcd57dfc546adfb15ac0a34c713d29280f
Jørgen Loe Kvalberg <jorgen.kvalberg@gmail.com>
2021-12-09 16:32:18 +0100

    Add base map layer enum description

--- src/app/core/models/basemap-layer.enum.ts
+++ src/app/core/models/basemap-layer.enum.ts
@@ -1,3 +1,10 @@
+/**
+ * IDs of basemap layers.
+ *
+ * We also have the BaseMap enum that defines the ids of base maps the user can choose among, where one base map may
+ * use one or more of these base map layers, eg. arcgis for everywhere and statensKartverk for norway.
+ * See settings.ts for how this relationship works.
+ */
 export enum BaseMapLayer {
   openTopo = 'openTopo',
   arcGisOnline = 'arcGisOnline',
