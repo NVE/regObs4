53f0b3ae6c4b0cdd8faa4d53a3c5cea5da8b8c2c
Jørgen Loe Kvalberg <jolokv@nve.no>
2021-12-12 18:21:59 +0100

    Log version info in sentry.service.ts (#143)
    Fjernet sirkulær refereanse som ble innført da vi forsøkte å logge versjonsnummer til fil ved oppstart. Dette feilet i produksjon fordi da er sentry servicen loggingservice, og sentry.service har en avhengighet til appversionservice. 
    Appversionservice hadde fått en avhengigjet til loggingservice.
    
    Co-authored-by: Jørgen Loe Kvalberg <jorgkv@users.noreply.github.com>

--- src/app/core/services/app-version/app-version.service.ts
+++ src/app/core/services/app-version/app-version.service.ts
@@ -1,18 +1,10 @@
 import { Injectable } from '@angular/core';
-import { LoggingService } from 'src/app/modules/shared/services/logging/logging.service';
 import { AppVersion } from '../../models/app-version.model';
-
-const DEBUG_TAG = 'AppVersionService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppVersionService {
-  constructor(private loggingService: LoggingService) {
-    const version = this.getAppVersion();
-    this.loggingService.debug(`Version = ${version.version}, build = ${version.buildNumber}`, DEBUG_TAG);
-  }
-
   getAppVersion(): AppVersion {
     try {
       // tslint:disable-next-line:no-var-requires
