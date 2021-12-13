3345cd28b2ae4f9fc3c981eeec462c545b6705e7
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2021-12-13 11:40:45 +0100

    Vise nedlastede kartpakker i oversiktsliste (#144)
    * Nå viser vi også nedlastede pakker i lista. Har fordelt teksten over 4 kolonner i stedet for 3 for å få plass selv på ganske smale skjermer. Plassforbruk for hver pakke er nå på linje med totalplassforbruk
    
    * Fiksa bekreftelses-popup som inneholdt teksten "Er du sikker?" 2 ganger
    
    * Add nynorsk
    
    Co-authored-by: Jørgen Loe Kvalberg <jorgen.kvalberg@gmail.com>

--- src/app/pages/offline-map/offline-map.page.ts
+++ src/app/pages/offline-map/offline-map.page.ts
@@ -45,9 +45,9 @@ interface PackageTotals {
 export class OfflineMapPage extends NgDestoryBase {
   private readonly installedPackages$: Observable<Map<string, OfflineMapPackage>>;
   private installedPackages: Map<string, OfflineMapPackage> = new Map();
-  downloadAndUnzipProgress$: Observable<OfflineMapPackage[]>;
+  private downloadAndUnzipProgress$: Observable<OfflineMapPackage[]>;
   packageTotals$: Observable<PackageTotals>;
-  private readonly allPackages$: Observable<OfflineMapPackage[]>;
+  readonly allPackages$: Observable<OfflineMapPackage[]>;
   private packagesOnServer$: Observable<Map<string, CompoundPackage>>;
   private packagesOnServer: Map<string, CompoundPackage> = new Map();
   showTileCard = true;
@@ -237,38 +237,41 @@ export class OfflineMapPage extends NgDestoryBase {
   }
 
   showPackageModalForPackage(map: OfflineMapPackage) {
-    const feature = this.featureMap.get(map.compoundPackageMetadata.getName());
-    if(feature) {
+    const feature = this.featureMap.get(map.name);
+    if (feature) {
       this.showPackageModal(feature.feature);
     }
   }
 
-  humanReadableByteSize(bytes: number): string {
+  humanReadableByteSize(bytes: number, fractionDigits = 0): string {
     if (isNaN(bytes)) {
       return '';
     }
-    return this.helperService.humanReadableByteSize(bytes, true);
+    return this.helperService.humanReadableByteSize(bytes, fractionDigits, true);
   }
 
-  getPercentage(map: OfflineMapPackage): number {
-    return Math.round((map.progress ? map.progress.percentage : 0) * 100);
+  formatProgressIfDownloading(map: OfflineMapPackage): string {
+    if (map.downloadStart && !map.downloadComplete) {
+      const value = Math.round((map.progress ? map.progress.percentage : 0) * 100);
+      return `(${value}%)`;
+    }
+    return '';
   }
 
   async cancelOrDelete(map: OfflineMapPackage, event: Event) {
     event.stopPropagation();
     if (this.isDownloaded(map)) {
-      const toTranslate = ['DIALOGS.ARE_YOU_SURE', 'DIALOGS.CANCEL', 'DIALOGS.OK'];
+      const toTranslate = ['OFFLINE_MAP.DELETE_PACKAGE_CONFIRM', 'DIALOGS.CANCEL', 'DIALOGS.DELETE'];
       const translations = await firstValueFrom(this.translateService.get(toTranslate));
       const alert = await this.alertController.create({
-        header: translations['DIALOGS.ARE_YOU_SURE'],
-        message: translations['DIALOGS.ARE_YOU_SURE'],
+        message: translations['OFFLINE_MAP.DELETE_PACKAGE_CONFIRM'],
         buttons: [
           {
             text: translations['DIALOGS.CANCEL'],
             role: 'cancel'
           },
           {
-            text: translations['DIALOGS.OK'],
+            text: translations['DIALOGS.DELETE'],
             handler: () => {
               this.offlineMapService.removeMapPackageByName(map.name);
             }
@@ -281,15 +284,11 @@ export class OfflineMapPage extends NgDestoryBase {
     }
   }
 
-  isDownloading(map: OfflineMapPackage): boolean {
-    return map.downloadStart && !map.downloadComplete;
-  }
-
   isDownloaded(map: OfflineMapPackage): boolean {
     return !!map.downloadComplete;
   }
 
   getSpaceAvailable(): string {
-    return this.humanReadableByteSize(this.offlineMapService.availableDiskspace?.available);
+    return this.humanReadableByteSize(this.offlineMapService.availableDiskspace?.available, 0);
   }
 }
