eb7b7ef71bfd9d26bfc7ccc76d8b3a78cbb80bd3
Jørgen Loe Kvalberg <jolokv@nve.no>
2022-01-28 13:15:18 +0100

    Add maps from npolar (#121)
    * Add maps from npolar
    
    * Add setting types and clean up settings

---
+++ src/app/core/models/maplayer-zindex.enum.ts
@@ -0,0 +1,8 @@
+export enum MapLayerZIndex {
+  OnlineMixedBackgroundLayer = 0,
+  OfflineBackgroundLayer = 10,
+  OnlineBackgroundLayer = 20,
+  OfflineSupportLayer = 30,
+  OnlineSupportLayer = 40,
+  Top = 50
+}
