1a3ebefc02df88cff704896654e73d26f587203a
omy <omy@nve.no>
2022-03-06 22:02:31 +0100

    Skrevet om skredproblem-registrering til ny kladdemodell

--- src/app/modules/registration/pages/snow/avalanche-problem/avalanche-problem.page.ts
+++ src/app/modules/registration/pages/snow/avalanche-problem/avalanche-problem.page.ts
@@ -14,6 +14,11 @@ import { KdvService } from '../../../../../core/services/kdv/kdv.service';
 import { Subscription } from 'rxjs';
 import { ArrayHelper } from 'src/app/core/helpers/array-helper';
 
+/**
+ * Start page / CRUD page for avalanche problems.
+ * Shows list of registered problems and offers to add new problems, edit exising problems or delete problems.
+ * You can also add pictures connected to avalanche problems.
+ */
 @Component({
   selector: 'app-avalanche-problem',
   templateUrl: './avalanche-problem.page.html',
@@ -53,41 +58,26 @@ export class AvalancheProblemPage extends BasePage {
   }
 
   async addOrEditAvalancheProblem(index?: number) {
-    if (
-      this.registration &&
-      this.registration.request &&
-      this.registration.request.AvalancheEvalProblem2
-    ) {
+    if (this.draft?.registration?.AvalancheEvalProblem2) {
       const modal = await this.modalController.create({
         component: AvalancheProblemModalPage,
         componentProps: {
-          avalancheEvalProblem: this.registration.request.AvalancheEvalProblem2[
-            index
-          ]
+          avalancheEvalProblem: this.draft.registration.AvalancheEvalProblem2[index]
         }
       });
       modal.present();
       const result = await modal.onDidDismiss();
       this.ngZone.run(() => {
-        if (
-          this.registration &&
-          this.registration.request &&
-          this.registration.request.AvalancheEvalProblem2
-        ) {
+        if (this.draft?.registration?.AvalancheEvalProblem2) {
           if (result.data) {
             if (result.data.delete) {
-              this.registration.request.AvalancheEvalProblem2.splice(index, 1);
+              this.draft.registration.AvalancheEvalProblem2.splice(index, 1);
             } else {
-              const avalancheEvalProblem: AvalancheEvalProblem2EditModel =
-                result.data;
+              const avalancheEvalProblem: AvalancheEvalProblem2EditModel = result.data;
               if (index !== undefined) {
-                this.registration.request.AvalancheEvalProblem2[
-                  index
-                ] = avalancheEvalProblem;
+                this.draft.registration.AvalancheEvalProblem2[index] = avalancheEvalProblem;
               } else {
-                this.registration.request.AvalancheEvalProblem2.push(
-                  avalancheEvalProblem
-                );
+                this.draft.registration.AvalancheEvalProblem2.push(avalancheEvalProblem);
               }
             }
           }
@@ -108,8 +98,8 @@ export class AvalancheProblemPage extends BasePage {
   }
 
   onProblemReorder(event: CustomEvent<ItemReorderEventDetail>): void {
-    this.registration.request.AvalancheEvalProblem2 = ArrayHelper.reorderList(
-      this.registration.request.AvalancheEvalProblem2,
+    this.draft.registration.AvalancheEvalProblem2 = ArrayHelper.reorderList(
+      this.draft.registration.AvalancheEvalProblem2,
       event.detail.from,
       event.detail.to
     );
