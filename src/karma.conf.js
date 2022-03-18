0f9ca2e24b3f7800d3b505e664bb2b6f9ec61fbf
Jørgen Loe Kvalberg <jorgen.kvalberg@gmail.com>
2022-03-18 12:00:10 +0100

    Fix RO-1590 unstable tests (#210)
    * Fix test causing no window to close err
    
    * Modify ci pipeline
    
      - Use npm cache
      - Use npm ci
      - Skip build step
      - Always try to publish test results

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,61 +1,70 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+/** @type { import('karma').ConfigOptions } */
+const configOptions = {
+  basePath: '',
+  frameworks: ['jasmine', '@angular-devkit/build-angular'],
+  port: 9876,
+  browsers: ['ChromeHeadless']
+};
 
+/** @param { import('karma').Config } config  */
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-junit-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageReporter: {
-      dir: '../coverage',
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'lcovonly' },
-        { type: 'cobertura' }
+  if (process.env['CI_TEST'] != null) {
+    config.set({
+      ...configOptions,
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-junit-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma'),
       ],
-      includeAllSources: true,
-      fixWebpackSourcePaths: true,
-      // check: {
-      //   global: {
-      //     statements: 50,
-      //     branches: 50,
-      //     functions: 50,
-      //     lines: 50
-      //   }
-      // }
-    },
-    reporters: ['progress', 'junit', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
-          '--remote-debugging-port=9222',
-        ]
+      autoWatch: false,
+      singleRun: true,
+      client: {
+        captureConsole: false,
+      },
+      reporters: [
+        'junit',
+        'coverage'
+      ],
+      coverageReporter: {
+        dir: '../coverage',
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'lcovonly' },
+          { type: 'cobertura' }
+        ],
+        includeAllSources: true,
+        fixWebpackSourcePaths: true,
+        // check: {
+        //   global: {
+        //     statements: 50,
+        //     branches: 50,
+        //     functions: 50,
+        //     lines: 50
+        //   }
+        // }
+      },
+      junitReporter: {
+        outputDir: 'test-reports',
+        suite: '',
+      },
+      colors: false,
+      logLevel: config.LOG_WARN
+    });
+  } else {
+    config.set({
+      ...configOptions,
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('@angular-devkit/build-angular/plugins/karma'),
+      ],
+      client: {
+        clearContext: false
       }
-    },
-    browsers: ['Chrome', 'ChromeHeadless'],
-    junitReporter: {
-      outputDir: 'test-reports',
-      suite: '',
-    },
-    singleRun: true
-  });
+    });
+  }
 };
