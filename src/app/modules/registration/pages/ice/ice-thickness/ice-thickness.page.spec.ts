18ce45c1e60abbe3dc96ad8e16497b5bb0484181
Aron Widforss <arwi@nve.no>
2022-03-29 09:40:23 +0200

    Add tests (#205)
    Fix validation of reset overwater

--- src/app/modules/registration/pages/ice/ice-thickness/ice-thickness.page.spec.ts
+++ src/app/modules/registration/pages/ice/ice-thickness/ice-thickness.page.spec.ts
@@ -1,27 +1,78 @@
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BasePageService } from '../../base-page-service';
+import { RegistrationTid } from 'src/app/modules/common-registration/registration.models';
 
-// import { IceThicknessPage } from './ice-thickness.page';
+import { IceThicknessPage } from './ice-thickness.page';
+import { GeoHazard } from 'src/app/modules/common-core/models';
 
-// describe('IceThicknessPage', () => {
-//   let component: IceThicknessPage;
-//   let fixture: ComponentFixture<IceThicknessPage>;
+describe('IceThicknessPage', () => {
+  let component: IceThicknessPage;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ IceThicknessPage ],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(() => {
+    let basePageService = new BasePageService(null, null, null, null,
+                                              null, null);
+    component = new IceThicknessPage(basePageService,
+                                     null, null, null);
+    component.draft = {
+      registration: {
+        DtObsTime: new Date(2020, 0, 1).toISOString(),
+        GeoHazardTID: GeoHazard.Ice,
+        IceThickness: {}
+      },
+      uuid: null,
+      syncStatus: null,
+    };
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(IceThicknessPage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it("empty icethickness is valid", () => {
+    expect(component.isValid()).toBeTrue();
+  });
+
+  it("isWaterBefore without waterheightbefore should be invalid", () => {
+    component.isWaterBefore = true;
+    expect(component.isValid()).toBeFalse();
+  });
+
+  it("isWaterAfter without waterheightafter should be invalid", () => {
+    component.isWaterAfter = true;
+    expect(component.isValid()).toBeFalse();
+  });
+
+  it("validity check shouldn't modify values", () => {
+    component.isWaterBefore = true;
+    component.isWaterAfter = true;
+    component.waterHeightBefore = 6;
+    component.waterHeightAfter = 10;
+    expect(component.isValid()).toBeTrue();
+    expect(component.waterHeightBefore).toEqual(6);
+    expect(component.waterHeightAfter).toEqual(10);
+  });
+
+  it("fills out registration correctly", () => {
+    let iceThickness = component.draft.registration.IceThickness;
+    component.isWaterBefore = undefined
+    component.waterHeightBefore = 10;
+    component.isWaterAfter = undefined
+    component.waterHeightAfter = 12;
+    component.waterDepthAfter = 15;
+    expect(component.isValid()).toBeTrue();
+    expect(iceThickness.IceHeightBefore).toBeUndefined();
+    expect(iceThickness.IceHeightAfter).toBeUndefined();
+
+    component.isWaterBefore = true;
+    component.isWaterAfter = true;
+    expect(component.isValid()).toBeTrue();
+    expect(iceThickness.IceHeightBefore).toEqual(-10);
+    expect(iceThickness.IceHeightAfter).toEqual(-12);
+
+    component.isWaterBefore = false;
+    component.isWaterAfter = false;
+    expect(component.isValid()).toBeTrue();
+    expect(iceThickness.IceHeightBefore).toEqual(0);
+    expect(iceThickness.IceHeightAfter).toEqual(15);
+
+  });
+});
