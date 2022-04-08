dcacc24a002a4f8c39d6d7dfc39a864491cf6de8
omy <omy@nve.no>
2022-04-08 08:44:27 +0200

    Fikset null-sjekk etter Jørgens kommentar

--- src/app/modules/registration/components/numeric-input/numeric-input.component.spec.ts
+++ src/app/modules/registration/components/numeric-input/numeric-input.component.spec.ts
@@ -17,6 +17,15 @@ describe('NumericInputComponent', () => {
 
     component.value = null;
     expect(component.displayValue).toBeUndefined();
+
+    component.value = 0;
+    expect(component.displayValue).toEqual('0');
+
+    component.convertRatio = 10;
+    component.value = 0;
+    expect(component.displayValue).toEqual('0');
+    component.value = 1;
+    expect(component.displayValue).toEqual('10');
   });
 
 });
