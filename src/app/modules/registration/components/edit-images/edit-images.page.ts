669d5b34372e009955d97b0e543772b2b79cae51
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2022-08-19 17:40:31 +0200

    Fikset slik at også bilder som er lastet opp til Regobs kan fjernes i enkel observasjon

--- src/app/modules/registration/components/edit-images/edit-images.page.ts
+++ src/app/modules/registration/components/edit-images/edit-images.page.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { RemoteOrLocalAttachmentEditModel } from 'src/app/core/services/draft/draft-model';
 import { GeoHazard } from 'src/app/modules/common-core/models';
 import { RegistrationTid } from 'src/app/modules/common-registration/registration.models';
 
 /**
- * A modal popup/page containing the EditImagesComponent
+ * A modal popup/page containing the EditImagesComponent.
+ * If existingAttachments is changed, they will be returned by onWillDismiss() on the modal object.
  */
 @Component({
   templateUrl: './edit-images.page.html',
@@ -15,7 +16,6 @@ import { RegistrationTid } from 'src/app/modules/common-registration/registratio
 export class EditImagesPage {
   @Input() draftUuid: string;
   @Input() existingAttachments: RemoteOrLocalAttachmentEditModel[];
-  @Output() existingAttachmentsChange = new EventEmitter();
   @Input() registrationTid: RegistrationTid;
   @Input() geoHazard: GeoHazard;
   @Input() modalTitlePostfix: string; //used to build the title in the modal
@@ -23,6 +23,6 @@ export class EditImagesPage {
   constructor(private modalController: ModalController) {}
 
   close() {
-    this.modalController.dismiss();
+    this.modalController.dismiss({ existingAttachments: this.existingAttachments});
   }
 }
--- src/app/modules/registration/components/snow/simple-snow-obs/images-editor/images-editor.component.ts
+++ src/app/modules/registration/components/snow/simple-snow-obs/images-editor/images-editor.component.ts
@@ -1,8 +1,15 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import deepEqual from 'fast-deep-equal';
+import { map, Observable } from 'rxjs';
 import { RegistrationDraft } from 'src/app/core/services/draft/draft-model';
+import { DraftRepositoryService } from 'src/app/core/services/draft/draft-repository.service';
 import { ExistingOrNewAttachment } from 'src/app/modules/common-registration/registration.models';
+import { SummaryItemService } from 'src/app/modules/registration/services/summary-item.service';
+import { LoggingService } from 'src/app/modules/shared/services/logging/logging.service';
 import { EditImagesPage } from '../../../edit-images/edit-images.page';
+
+const DEBUG_TAG = 'ImagesEditorComponent';
 
 /**
  * Show thumbnails of all images for given registration.
@@ -18,12 +25,23 @@ export class ImagesEditorComponent {
    @Input() draft: RegistrationDraft;
    @Input() registrationTid: number;
    @Input() modalTitlePostfix: string; //used to build the title in the modal
-   @Input() attachments: ExistingOrNewAttachment[]; //attachments for given draft registration
    @Input() readonly = false;
 
+   attachments$: Observable<ExistingOrNewAttachment[]>;
+
    constructor(
-    private modalController: ModalController
+    private modalController: ModalController,
+    private summaryItemService: SummaryItemService,
+    private draftRepository: DraftRepositoryService,
+    private logger: LoggingService
    ) {
+   }
+
+   ngOnInit() {
+     this.attachments$ = this.summaryItemService.createAttachments$(this.draft.uuid).pipe(
+       map(attachments => attachments.filter(
+         existingOrNewAttachment => existingOrNewAttachment.attachment.RegistrationTID === this.registrationTid))
+     );
    }
 
    async showImagesEditorModal(): Promise<void> {
@@ -34,11 +52,24 @@ export class ImagesEditorComponent {
          geoHazard: this.draft.registration.GeoHazardTID,
          draftUuid: this.draft.uuid,
          modalTitlePostfix: this.modalTitlePostfix,
-         existingAttachments: this.draft.registration.Attachments //TODO: Må denne filtreres på riktig skjema?
+         existingAttachments: this.draft.registration.Attachments
        }
      });
-     modal.present();
-   }
+     await modal.present();
 
+     const { data } = await modal.onWillDismiss();
+     if (data != null && (!deepEqual(data.existingAttachments, this.draft.registration.Attachments))) {
+       this.logger.debug(
+         'Existing (remote) attachments changed, saving draft...',
+         DEBUG_TAG,
+         {'changed': data.existingAttachments},
+         {'original': this.draft.registration.Attachments}
+       );
+       this.draft.registration.Attachments = data.existingAttachments;
+       this.draftRepository.save(this.draft);
+     } else {
+       this.logger.debug('Existing (remote) attachments not changed', DEBUG_TAG);
+     }
+   }
 }
 
