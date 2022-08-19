c2b6137514c601403282be2c0118e01a6ae53d3f
robinzaal <robin.zaal@gmail.com>
2022-08-19 17:02:19 +0200

    Make sure images for registration are square

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,50 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module'
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
-  'plugins': [
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'no-warning-comments': [
-      'warn',
-      { 'terms': ['todo', 'fixme'], 'location': 'start' }
-    ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-warning-comments': ['warn', { terms: ['todo', 'fixme'], location: 'start' }],
     'no-console': 'error',
     'no-trailing-spaces': ['error'],
     'no-multiple-empty-lines': [
       'warn',
       {
-        'max': 2
+        max: 2
       }
     ],
-    'indent': [
-      'error',
-      2
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'max-len': [
       'warn',
       {
-        'ignorePattern': '^import |// import',
-        'code': 120
+        ignorePattern: '^import |// import',
+        code: 120
       }
-    ],
-  }
+    ]
+  },
+  overrides: [
+    {
+      files: ['*.component.html'],
+      parser: '@angular-eslint/template-parser',
+      rules: {
+        '@typescript-eslint/dot-notation': 'off',
+        '@typescript-eslint/no-implied-eval': 'off',
+        '@typescript-eslint/no-throw-literal': 'off',
+        strict: 'off',
+        'import/first': 'off',
+        'lines-around-directive': 'off'
+      },
+      plugins: ['@angular-eslint/template']
+    }
+  ]
 };
