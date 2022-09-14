d0fff7241d6bd9ba750b14be9b4638b16029d9be
Jørgen Loe Kvalberg <j@kvalberg.no>
2022-09-14 15:49:47 +0200

    Temp commit for tests

--- src/app/core/services/observer-trips/observer-trips.spec.ts
+++ src/app/core/services/observer-trips/observer-trips.spec.ts
@@ -1,5 +1,6 @@
-import { fakeAsync, flush, tick } from '@angular/core/testing';
-import { BehaviorSubject, firstValueFrom, ReplaySubject } from 'rxjs';
+import { discardPeriodicTasks, fakeAsync, flush, flushMicrotasks, resetFakeAsyncZone, tick } from '@angular/core/testing';
+import { BehaviorSubject, firstValueFrom, of, ReplaySubject, timeout } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
 import { RegobsAuthService } from 'src/app/modules/auth/services/regobs-auth.service';
 import { TripService } from 'src/app/modules/common-regobs-api';
 import { LoggedInUser } from 'src/app/modules/login/models/logged-in-user.model';
@@ -12,11 +13,14 @@ describe('ObserverTripsService', () => {
   let loggerService: jasmine.SpyObj<LoggingService>;
   let database: any;
   let dbService: DatabaseService;
+  let dbServiceGetSpy: jasmine.Spy;
+  let dbServiceSetSpy: jasmine.Spy;
   let loggedInUser: ReplaySubject<LoggedInUser>;
   let authService: jasmine.SpyObj<RegobsAuthService>;
   let service: ObserverTripsService;
 
   beforeEach(() => {
+    resetFakeAsyncZone();
     tripService = jasmine.createSpyObj('TripService', ['TripGet']);
     loggerService = jasmine.createSpyObj('LoggerService', ['debug', 'error']);
     database = {};
@@ -24,8 +28,8 @@ describe('ObserverTripsService', () => {
       get: async (key) => database[key],
       set: async (key, val) => database[key] = val
     } as DatabaseService;
-    spyOn(dbService, 'get');
-    spyOn(dbService, 'set');
+    dbServiceGetSpy = spyOn(dbService, 'get').and.callThrough();
+    dbServiceSetSpy = spyOn(dbService, 'set').and.callThrough();
     loggedInUser = new ReplaySubject<LoggedInUser>();
     authService = jasmine.createSpyObj('RegobsAuthService', {}, { loggedInUser$: loggedInUser.asObservable() });
     service = new ObserverTripsService(tripService, loggerService, dbService, authService);
@@ -33,10 +37,10 @@ describe('ObserverTripsService', () => {
 
   it('should not fetch data if it has valid data saved in database', fakeAsync(async () => {
     database = {
-      isAuthorizedKey: true,
-      toggleKey: true,
-      dataKey: { features: true },
-      dataModifiedKey: Date.now()
+      [isAuthorizedKey]: true,
+      [toggledOnKey]: true,
+      [dataKey]: { features: true },
+      [dataModifiedKey]: Date.now()
     };
 
     loggedInUser.next({ isLoggedIn: true });
@@ -46,30 +50,94 @@ describe('ObserverTripsService', () => {
     // Let stuff happen
     tick(100);
 
+    // It should not be needed to write anything to the db in this scenario
+    expect(dbServiceSetSpy).toHaveBeenCalledTimes(0);
+
+    // Check that service checks the authorized value and reads data from database
+    // TODO: Remove
+    const dbArgs = dbServiceGetSpy.calls.allArgs();
+    expect(dbArgs).toContain([isAuthorizedKey]);
+    expect(dbArgs).toContain([dataKey]);
+    expect(dbArgs).toContain([dataModifiedKey]);
+
+    // Check that tripService was not called
+    expect(tripService.TripGet).toHaveBeenCalledTimes(0);
+
     expect(geojson.features).toBeTrue();
   }));
 
-  it('it should not do anything until a user is logged in', fakeAsync(() => {
+  it('should fetch data if data is too old', fakeAsync(async () => {
+    // "as null" because TripGet return type is Observable<null> but it returns an observable with geojson data
+    tripService.TripGet.and.returnValue(of({ features: [{ type: 'NewFeature1' }, { type: 'NewFeature2' }] } as null));
+
+    database = {
+      [isAuthorizedKey]: true,
+      [toggledOnKey]: true,
+      [dataKey]: { features: [{ type: 'OldFeature1' }, { type: 'OldFeature1' }, { type: 'OldFeature1' }] },
+      [dataModifiedKey]: 100
+    };
+
+    loggedInUser.next({ isLoggedIn: true });
+
+    // Something must subscribe for stuff to happen
+    const geojson = await firstValueFrom(service.geojson$);
+
+    // Let time pass
+    tick(100);
+
+    // Check that tripService was called to fetch new data
+    expect(tripService.TripGet).toHaveBeenCalledTimes(1);
+    expect(geojson.features).toHaveSize(2);
+    expect(geojson.features[0].type).toBe('NewFeature1');
+  }));
+
+  it('should use old data if fetching new data fails', fakeAsync(async () => {
+    // "as null" because TripGet return type is Observable<null> but it returns an observable with geojson data
+    tripService.TripGet.and.throwError('Failed to fetch new data');
+
+    database = {
+      [isAuthorizedKey]: true,
+      [toggledOnKey]: true,
+      [dataKey]: { features: [{ type: 'OldFeature1' }, { type: 'OldFeature1' }, { type: 'OldFeature1' }] },
+      [dataModifiedKey]: 100
+    };
+
+    loggedInUser.next({ isLoggedIn: true });
+
+    // Something must subscribe for stuff to happen
+    const geojson = await firstValueFrom(service.geojson$);
+
+    // Let time pass
+    tick(100);
+
+    // Check that tripService was called to try fetching new data
+    expect(tripService.TripGet).toHaveBeenCalledTimes(1);
+    expect(geojson.features[0].type).toBe('OldFeature1');
+  }));
+
+  it('it should not do anything until a user is logged in', fakeAsync(async () => {
+    // Subscribe to geojson so logic in service starts
+    const geojsonPromise = firstValueFrom(service.geojson$.pipe(timeout(1000)));
+
     // Let some time pass
     tick(100);
 
-    // We should have asked for the logged in user
-    expect(Object.getOwnPropertyDescriptor(authService, 'loggedInUser$').get).toHaveBeenCalled();
-
     // But nothing else should have happened
     expect(tripService.TripGet).toHaveBeenCalledTimes(0);
-    expect(dbService.get).toHaveBeenCalledTimes(0);
-    expect(dbService.set).toHaveBeenCalledTimes(0);
+    expect(dbServiceGetSpy).toHaveBeenCalledTimes(0);
 
     // emit something else than a logged in user
     loggedInUser.next({ email: null, token: null, isLoggedIn: false });
 
-    // Let some time pass
-    tick(100);
+    // Let timeout complete and promise to reject
+    try {
+      tick(2000);
+      await geojsonPromise;
+    // eslint-disable-next-line no-empty
+    } catch (error) {}
 
-    // Still should not have called any other dependencies
+    // Still should not have called any dependencies
     expect(tripService.TripGet).toHaveBeenCalledTimes(0);
-    expect(dbService.get).toHaveBeenCalledTimes(0);
-    expect(dbService.set).toHaveBeenCalledTimes(0);
+    expect(dbServiceGetSpy).toHaveBeenCalledTimes(0);
   }));
 });
