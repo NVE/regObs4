c1b28d79368c120ae44c6d3ccf72569ed382ba5c
Jørgen Loe Kvalberg <jolokv@nve.no>
2022-10-20 15:12:48 +0200

    Use capacitor keyboard plugin (#338)
    * Remove button property on ion-item
    
    * Show "Done" button on ios keyboard

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -21,7 +21,6 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { StartWizardGuard } from './core/guards/start-wizard.guard';
 import { DataMarshallService } from './core/services/data-marshall/data-marshall.service';
 import { AuthGuard } from './core/guards/auth.guard';
-import { Keyboard } from '@ionic-native/keyboard/ngx';
 import { SQLite } from '@ionic-native/sqlite/ngx';
 import { Network } from '@ionic-native/network/ngx';
 import { LoggingService } from './modules/shared/services/logging/logging.service';
@@ -109,7 +108,6 @@ export const APP_PROVIDERS = [
   HTTP,
   WebView,
   EmailComposer,
-  Keyboard,
   SQLite,
   Network,
   ScreenOrientation,
