0158ae47f8aa84f1fb2267a20b4f50e44e5b0fd4
Jørgen Loe Kvalberg <j@kvalberg.no>
2022-12-19 14:51:26 +0100

    Add danish language

--- src/app/modules/common-regobs-api/models/search-side-bar-request-dto.ts
+++ src/app/modules/common-regobs-api/models/search-side-bar-request-dto.ts
@@ -11,5 +11,5 @@ export interface SearchSideBarRequestDto {
    * NO = 1, EN = 2, DE = 3, SL = 4, SV = 5, IT = 6
    * Default = 2.
    */
-  LangKey?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+  LangKey?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 }
