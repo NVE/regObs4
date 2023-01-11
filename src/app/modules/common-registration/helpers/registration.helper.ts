f47ab0a5bb128bf85e706f882221507db8f76b63
Jørgen Loe Kvalberg <j@kvalberg.no>
2023-01-11 16:17:45 +0100

    Move getUniqueRegistrations to helper file

--- src/app/modules/common-registration/helpers/registration.helper.ts
+++ src/app/modules/common-registration/helpers/registration.helper.ts
@@ -201,3 +201,19 @@ function getDefaultValue(registrationTid: RegistrationTid) {
     return {};
   }
 }
+
+/**
+ * Return a list with only unique registrations.
+ *
+ * Only uses RegID when comparing registrations.
+ */
+export function getUniqueRegistrations<T extends Pick<RegistrationViewModel, 'RegId'>>(regs: T[]) {
+  const regIds = new Set();
+  return regs.filter((reg) => {
+    if (regIds.has(reg.RegId)) {
+      return false;
+    }
+    regIds.add(reg.RegId);
+    return true;
+  });
+};
