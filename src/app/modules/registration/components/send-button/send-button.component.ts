187add7600ab9429830bfbfca908f811d4633957
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2023-02-14 16:14:44 +0100

    Endre tekst på innsendingsknapp
    avhengig av om man er logget inn eller ikke

--- src/app/modules/registration/components/send-button/send-button.component.ts
+++ src/app/modules/registration/components/send-button/send-button.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
-import { map, startWith, switchMap } from 'rxjs/operators';
+import { map, startWith, switchMap, takeUntil } from 'rxjs/operators';
 import { RegobsAuthService } from '../../../auth/services/regobs-auth.service';
 import { combineLatest, firstValueFrom, Observable, Subject } from 'rxjs';
 import { RegistrationDraft } from 'src/app/core/services/draft/draft-model';
@@ -14,6 +14,7 @@ import {
   ConfirmationModalService,
   PopupResponse,
 } from '../../../../core/services/confirmation-modal/confirmation-modal.service';
+import { NgDestoryBase } from 'src/app/core/helpers/observable-helper';
 
 const DEBUG_TAG = 'SendButtonComponent';
 const DELETE_OBS_TIMEOUT_MS = 5000;
@@ -24,10 +25,11 @@ const DELETE_OBS_TIMEOUT_MS = 5000;
   styleUrls: ['./send-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SendButtonComponent implements OnInit, OnChanges {
+export class SendButtonComponent extends NgDestoryBase implements OnInit, OnChanges {
   @Input() draft: RegistrationDraft;
 
   isDisabled$: Observable<boolean>;
+  caption$: Observable<string>; // submit button caption
   private isSending = new Subject<boolean>();
   private hasChanges = new Subject<void>();
 
@@ -42,7 +44,26 @@ export class SendButtonComponent implements OnInit, OnChanges {
     private logger: LoggingService,
     private newAttachmentService: NewAttachmentService,
     private confirmationModalService: ConfirmationModalService
-  ) {}
+  ) {
+    super();
+    this.caption$ = regobsAuthService.loggedInUser$.pipe(
+      takeUntil(this.ngDestroy$),
+      map((user) => {
+        if (user.isLoggedIn) {
+          if (this.draft.error) {
+            return 'RESEND';
+          } else {
+            return 'SEND';
+          }
+        } else if (this.draft.error) {
+          return 'LOG_IN_AND_RESEND';
+        } else {
+          return 'LOG_IN_AND_SEND';
+        }
+      }),
+      switchMap((key) => this.translateService.get(`REGISTRATION.${key}`))
+    );
+  }
 
   ngOnInit(): void {
     const isEmpty$ = combineLatest([
