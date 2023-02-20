26cf6ea2c5502c03db17d2335ae6830de179969e
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2023-02-20 12:17:07 +0100

    RO-2160: Feilet bilde opplasting stopper oppretting av en ny observasjon (#452)
    * test
    
    * removed svg from possible upload
    
    * general message
    
    * comments resolved
    
    * addAttachment not called on attachment undefined

--- src/app/core/services/upload-attachments/upload-attachments.service.ts
+++ src/app/core/services/upload-attachments/upload-attachments.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient, HttpErrorResponse, HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { filter, firstValueFrom, map, Observable, tap } from 'rxjs';
 import { AttachmentUploadEditModel } from 'src/app/modules/common-registration/registration.models';
 import { NewAttachmentService } from 'src/app/modules/common-registration/registration.services';
 import { AttachmentService as ApiAttachmentService } from 'src/app/modules/common-regobs-api';
+import { DateHelperService } from 'src/app/modules/shared/services/date-helper/date-helper.service';
 import { LoggingService } from 'src/app/modules/shared/services/logging/logging.service';
 import { RegistrationDraft } from '../draft/draft-model';
 import { UserSettingService } from '../user-setting/user-setting.service';
@@ -37,8 +40,11 @@ export class UploadAttachmentsService {
     private httpClient: HttpClient,
     private newAttachmentService: NewAttachmentService,
     private apiAttachmentService: ApiAttachmentService,
+    private translateService: TranslateService,
+    private dateHelperService: DateHelperService,
     private loggingService: LoggingService,
-    private userSettings: UserSettingService
+    private userSettings: UserSettingService,
+    private alertController: AlertController
   ) {}
 
   /**
@@ -71,16 +77,7 @@ export class UploadAttachmentsService {
       try {
         return await this.uploadAttachment(attachment, draft);
       } catch (error) {
-        if (error instanceof HttpErrorResponse && error.status === 0) {
-          // Probably no network, stop trying to upload any attachments
-          throw error;
-        }
-
-        this.loggingService.error(error, DEBUG_TAG, 'Failed to upload attachment', attachment.AttachmentId);
-        failedAttachments.push({
-          id: attachment.id,
-          error,
-        });
+        this.uploadAttachmentHandleError(attachment, error, failedAttachments);
       }
     };
 
@@ -88,10 +85,47 @@ export class UploadAttachmentsService {
     const uploadedAttachments = await Promise.all(attachmentsToUpload.map(uploadAttachmentAndHandleErrors));
 
     if (failedAttachments.length) {
-      throw new UploadAttachmentError(draft.uuid, failedAttachments);
+      // If one of the attachment uploads fails we keep on sending the registration, tho we inform the user with a toast
+      this.showCouldnotUploadAllImagesAlert(failedAttachments.length, draft.registration.DtObsTime);
     }
 
     return [...alreadyUploaded, ...uploadedAttachments];
+  }
+
+  private uploadAttachmentHandleError(
+    attachment: AttachmentUploadEditModel,
+    error: Error,
+    failedAttachments: FailedAttachment[]
+  ) {
+    if (error instanceof HttpErrorResponse && error.status === 0) {
+      // Probably no network, stop trying to upload any attachments
+      throw error;
+    }
+
+    this.loggingService.error(error, DEBUG_TAG, 'Failed to upload attachment', attachment.AttachmentId);
+    failedAttachments.push({
+      id: attachment.id,
+      error,
+    });
+  }
+
+  private async showCouldnotUploadAllImagesAlert(failedAttachmentsCount: number, date: string) {
+    const dateProperFormat = await this.dateHelperService.formatDateString(date);
+    const translations = await firstValueFrom(
+      this.translateService.get(['REGISTRATION.IMAGE_UPLOAD_ERROR', 'ALERT.OK'], {
+        failedAttachmentsCount: failedAttachmentsCount,
+        obsTime: dateProperFormat,
+      })
+    );
+    const alert = await this.alertController.create({
+      message: translations['REGISTRATION.IMAGE_UPLOAD_ERROR'],
+      buttons: [
+        {
+          text: translations['ALERT.OK'],
+        },
+      ],
+    });
+    alert.present();
   }
 
   // TODO: Add test
