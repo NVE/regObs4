91a94b58e4c230866b6a8ab8508a93dfafaf51fd
Jørgen Loe Kvalberg <j@kvalberg.no>
2023-02-27 12:21:00 +0100

    Use SearchService to fetch reg for detail page

--- src/app/pages/view-observation/view-observation.page.ts
+++ src/app/pages/view-observation/view-observation.page.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { UserSettingService } from '../../core/services/user-setting/user-setting.service';
 import { RegistrationViewModel } from 'src/app/modules/common-regobs-api/models';
 import { PopupInfoService } from '../../core/services/popup-info/popup-info.service';
 import { NgDestoryBase } from '../../core/helpers/observable-helper';
-import { takeUntil, switchMap } from 'rxjs/operators';
-import { from, Observable } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { EditMode } from 'src/app/modules/registration/edit-registration-helper-functions';
-import { RegistrationService } from 'src/app/modules/common-regobs-api';
-import { Capacitor } from '@capacitor/core';
-import { SqliteService } from 'src/app/core/services/sqlite/sqlite.service';
+import { SearchService } from 'src/app/modules/common-regobs-api';
 
 @Component({
   selector: 'app-view-observation',
@@ -23,10 +20,8 @@ export class ViewObservationPage extends NgDestoryBase implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private registrationService: RegistrationService,
-    private sqliteService: SqliteService,
-    private userSettingService: UserSettingService,
-    private popupInfoService: PopupInfoService
+    private popupInfoService: PopupInfoService,
+    private searchService: SearchService
   ) {
     super();
   }
@@ -34,18 +29,10 @@ export class ViewObservationPage extends NgDestoryBase implements OnInit {
   ngOnInit() {
     this.popupInfoService.checkObservationInfoPopup().pipe(takeUntil(this.ngDestroy$)).subscribe();
     const regId = parseInt(this.activatedRoute.snapshot.params['id'], 10);
-    Capacitor.isNativePlatform() ? this.getRegistrationFromLocalDb(regId) : this.getRegistrationFromApi(regId);
-  }
-
-  getRegistrationFromApi(regId: number) {
-    this.registrationViewModel$ = this.userSettingService.language$.pipe(
-      switchMap((langKey) => this.registrationService.RegistrationGet({ regId, langKey }))
-    );
-  }
-
-  getRegistrationFromLocalDb(regId: number) {
-    this.registrationViewModel$ = this.userSettingService.userSetting$.pipe(
-      switchMap((userSetting) => from(this.sqliteService.loadRegistration(regId, userSetting.appMode)))
-    );
+    this.registrationViewModel$ = this.searchService
+      .SearchSearch({
+        RegId: regId,
+      })
+      .pipe(map((result) => result[0]));
   }
 }
