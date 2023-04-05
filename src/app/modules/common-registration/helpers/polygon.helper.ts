bc642d711038bca48983f80d3c99dc4f231652e3
marcin <martin.joodd@gmail.com>
2023-04-05 12:25:17 +0200

    code refactoring

---
+++ src/app/modules/common-registration/helpers/polygon.helper.ts
@@ -0,0 +1,59 @@
+import L from 'leaflet';
+import { IPolygon, Polygon, PolygonArea } from '../../registration/models/polygon';
+
+type PolygonType = 'total' | 'start' | 'end';
+
+export function constructPolygon(extent: [number, number][], color: string, Type: typeof Polygon, title?: string) {
+  return {
+    title,
+    active: Boolean(extent),
+    polygon: extent
+      ? new Type(
+          extent.map(([lng, lat]) => [lat, lng]),
+          { color }
+        )
+      : null,
+    color,
+  };
+}
+
+export function makePolygons(
+  polygonType: PolygonType,
+  polygon: IPolygon,
+  relativeToLatLng?: L.LatLng,
+  start?: L.LatLng,
+  end?: L.LatLng
+): IPolygon {
+  if (!polygon?.polygon) {
+    const totalCircle =
+      start && end
+        ? createCircleWithStartAndEnd(polygonType, start, end)
+        : createCircleWithoutStartEnd(relativeToLatLng);
+    polygon.polygon = new PolygonArea(L.PM.Utils.circleToPolygon(totalCircle, 5).getLatLngs());
+  }
+  return polygon;
+}
+
+const createCircleWithStartAndEnd = (polygonType: PolygonType, start: L.LatLng, end: L.LatLng): L.Circle => {
+  switch (polygonType) {
+    case 'total':
+      return new L.Circle([(start.lat + end.lat) / 2, (start.lng + end.lng) / 2], {
+        radius: start.distanceTo(end) / 2,
+      });
+    case 'start':
+      return new L.Circle([start.lat + (end.lat - start.lat) / 6, start.lng + (end.lng - start.lng) / 6], {
+        radius: start.distanceTo(end) / 6,
+      });
+    case 'end':
+      return new L.Circle([end.lat + (start.lat - end.lat) / 6, end.lng + (start.lng - end.lng) / 6], {
+        radius: start.distanceTo(end) / 6,
+      });
+  }
+};
+
+const createCircleWithoutStartEnd = (relativeToLatLng: L.LatLng): L.Circle => {
+  const fallbackLatlng = L.latLng(59.1, 10.3);
+  return new L.Circle(relativeToLatLng || fallbackLatlng, {
+    radius: 150,
+  });
+};
--- src/app/modules/registration/models/polygon.ts
+++ src/app/modules/registration/models/polygon.ts
@@ -1,43 +1,23 @@
-import * as L from "leaflet";
-import "leaflet-draw"
-import { settings } from "src/settings";
+import * as L from 'leaflet';
+import 'leaflet-draw';
+import { settings } from 'src/settings';
 
 export const Polygon = L.Polygon.extend({
   options: {
     poly: {
       allowIntersection: false,
-    }
-  }
+    },
+  },
 }) as unknown as typeof L.Polygon;
 
-export const TotalPolygon = L.Polygon.extend({
+export const PolygonArea = L.Polygon.extend({
   options: {
     poly: {
       drawError: {
-        color: settings.map.extentColor
-      }
-    }
-  }
-}) as unknown as typeof L.Polygon;
-
-export const StartPolygon = L.Polygon.extend({
-  options: {
-    poly: {
-      drawError: {
-        color: settings.map.startExtentColor
-      }
-    }
-  }
-}) as unknown as typeof L.Polygon;
-
-export const EndPolygon = L.Polygon.extend({
-  options: {
-    poly: {
-      drawError: {
-        color: settings.map.endExtentColor
-      }
-    }
-  }
+        color: settings.map.extentColor,
+      },
+    },
+  },
 }) as unknown as typeof L.Polygon;
 
 export interface IPolygon {
@@ -45,7 +25,7 @@ export interface IPolygon {
   active: boolean;
   polygon: L.Polygon & {
     editing?: L.Draw.Polygon;
-    options?: Partial<L.EditToolbar.EditHandlerOptions>
+    options?: Partial<L.EditToolbar.EditHandlerOptions>;
   };
   color: string;
-};
+}
