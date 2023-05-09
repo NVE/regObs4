950a867337387294511122952dea5f64ec5db3d4
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2023-05-09 11:34:18 +0200

    RO-2340: fjerne tom WaterLvl2 objekt fra api body hvis den ikke trengs (#547)
    * rmeoved waterlvl2 from obs if not needed
    
    * remvoved generalobservation from draft if empty
    
    * comments

--- src/app/modules/registration/components/water/simple-water-obs/simple-water-obs.component.ts
+++ src/app/modules/registration/components/water/simple-water-obs/simple-water-obs.component.ts
@@ -2,7 +2,9 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { RegistrationDraft } from 'src/app/core/services/draft/draft-model';
 import { DraftRepositoryService } from 'src/app/core/services/draft/draft-repository.service';
-import { GeneralObservationEditModel, Waterlevel2EditModel } from 'src/app/modules/common-regobs-api';
+import { isObservationModelEmptyForRegistrationTid } from 'src/app/modules/common-registration/registration.helpers';
+import { RegistrationTid } from 'src/app/modules/common-registration/registration.models';
+import { GeneralObservationEditModel, UrlEditModel, Waterlevel2EditModel } from 'src/app/modules/common-regobs-api';
 
 /**
  * Simplified water registration schema.'
@@ -19,21 +21,41 @@ export class SimpleWaterObsComponent {
   constructor(private draftRepository: DraftRepositoryService, private navController: NavController) {}
 
   get waterLevel2(): Waterlevel2EditModel {
-    if (!this.draft.registration.WaterLevel2) {
-      this.draft.registration.WaterLevel2 = {};
-    }
     return this.draft.registration.WaterLevel2;
   }
 
   get generalObservation(): GeneralObservationEditModel {
     if (!this.draft.registration.GeneralObservation) {
-      this.draft.registration.GeneralObservation = {};
+      return {};
     }
     return this.draft.registration.GeneralObservation;
   }
 
+  async saveComment(value: string) {
+    if (!this.draft.registration.GeneralObservation) {
+      this.draft.registration.GeneralObservation = {};
+    }
+    this.draft.registration.GeneralObservation.Comment = value;
+    await this.save();
+  }
+
+  async saveUrl(value: UrlEditModel[]) {
+    if (!this.draft.registration.GeneralObservation) {
+      this.draft.registration.GeneralObservation = {};
+    }
+    this.draft.registration.GeneralObservation.Urls = value;
+    await this.save();
+  }
+
   async save(): Promise<void> {
-    this.draftRepository.save(this.draft);
+    const isEmpty = isObservationModelEmptyForRegistrationTid(
+      this.draft.registration,
+      RegistrationTid.GeneralObservation
+    );
+    if (isEmpty) {
+      this.draft.registration.GeneralObservation = null;
+    }
+    await this.draftRepository.save(this.draft);
   }
 
   nav() {
