037ae516cb22bcb59e477bc8aaa4a918ba5d0cf7
omy <omy@nve.no>
2023-09-06 15:53:49 +0200

    Fjernet kode som kom inn ved en feil
    og oppdaget av Jørgen

--- src/app/pages/observation-list/observation-list.page.ts
+++ src/app/pages/observation-list/observation-list.page.ts
@@ -101,12 +101,6 @@ export class ObservationListPage extends NgDestoryBase implements OnInit {
       )
       .subscribe();
 
-    mapService.mapView$.pipe(
-      startWith({ bounds: null }), // In case mapService.MapView does not emit on startup
-      map((mapView) => mapView?.bounds == null),
-      distinctUntilChanged()
-    );
-
     const selectedRegionsFilterActive$ = this.searchCriteriaService.searchCriteria$.pipe(
       map((criteria) => (criteria.SelectedRegions || []).length > 0)
     );
