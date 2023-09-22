b5cdff3fd2d834768ee64c74cc5defa81425803b
Jørgen Loe Kvalberg <jolokv@nve.no>
2023-09-22 11:42:24 +0200

    RO-2488: Add login button from detail page (#598)
    * Add login button from detail page
    
    * Fix small bug if user logged out

--- src/app/pages/view-observation/view-observation.page.ts
+++ src/app/pages/view-observation/view-observation.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RegistrationViewModel } from 'src/app/modules/common-regobs-api/models';
 import { PopupInfoService } from '../../core/services/popup-info/popup-info.service';
 import { NgDestoryBase } from '../../core/helpers/observable-helper';
-import { takeUntil, map, catchError } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { takeUntil, map, catchError, filter } from 'rxjs/operators';
+import { Observable, Subject, merge, of } from 'rxjs';
 import { EditMode } from 'src/app/modules/registration/edit-registration-helper-functions';
 import { SearchService } from 'src/app/modules/common-regobs-api';
+import { RegobsAuthService } from 'src/app/modules/auth/services/regobs-auth.service';
 
 interface RegistrationResult {
   reg?: RegistrationViewModel;
@@ -23,15 +24,40 @@ export class ViewObservationPage extends NgDestoryBase implements OnInit {
   editMode$: Observable<EditMode>;
   registrationViewModel$: Observable<RegistrationResult>;
 
+  loggedInUser$: Observable<string>;
+  isLoggingIn$: Observable<boolean>;
+  private _isLoggingIn = new Subject<boolean>();
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private popupInfoService: PopupInfoService,
-    private searchService: SearchService
+    private searchService: SearchService,
+    private authService: RegobsAuthService,
+    private router: Router
   ) {
     super();
   }
 
+  goToMyPage() {
+    // we need to store curLocation in case user logs out and login on the /login page
+    // then we navigate based on prevUrl in localStorage otherwise back button will navigate to
+    // authcalback site and hang the app
+    const curLocation = this.router.url;
+    localStorage.setItem('prevUrl', curLocation);
+
+    this.router.navigateByUrl('/login');
+  }
+
+  async signIn() {
+    this._isLoggingIn.next(true);
+    await this.authService.signIn();
+  }
+
   ngOnInit() {
+    this.isLoggingIn$ = merge(this._isLoggingIn, this.authService.isLoggingIn$);
+
+    this.loggedInUser$ = this.authService.loggedInUser$.pipe(map((user) => (user.isLoggedIn ? user.email : null)));
+
     this.popupInfoService.checkObservationInfoPopup().pipe(takeUntil(this.ngDestroy$)).subscribe();
     const id = this.activatedRoute.snapshot.params['id'];
     const regId = parseInt(id, 10);
