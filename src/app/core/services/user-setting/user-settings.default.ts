973b5f2d5541f0c06931b16fd2b3d4dc0ecc16f0
Jørgen Loe Kvalberg <j@kvalberg.no>
2023-10-10 14:32:57 +0200

    Use usersettings

--- src/app/core/services/user-setting/user-settings.default.ts
+++ src/app/core/services/user-setting/user-settings.default.ts
@@ -1,6 +1,9 @@
 import { UserSetting } from '../../models/user-settings.model';
 import { GeoHazard, LangKey, AppMode } from 'src/app/modules/common-core/models';
 import { TopoMap } from '../../models/topo-map.enum';
+import { Capacitor } from '@capacitor/core';
+
+const isNative = Capacitor.isNativePlatform();
 
 export const DEFAULT_USER_SETTINGS: (langKey: LangKey) => UserSetting = (langKey: LangKey) => ({
   appMode: AppMode.Prod,
@@ -12,12 +15,13 @@ export const DEFAULT_USER_SETTINGS: (langKey: LangKey) => UserSetting = (langKey
     { geoHazard: GeoHazard.Soil, daysBack: 3 },
     { geoHazard: GeoHazard.Water, daysBack: 3 },
   ],
-  completedStartWizard: false,
+  completedStartWizard: !isNative,
   supportTiles: [],
   showMapCenter: true,
   showObservations: true,
   topoMap: TopoMap.default,
-  showGeoSelectInfo: true,
+  // showGeoSelectInfo er om coach marks skal vises etter oppstart / start-wizard
+  showGeoSelectInfo: isNative,
   completedSimpleObsOnboarding: false,
   useRetinaMap: false,
   featureToggeGpsDebug: false,
