932f23bf8f3b9c8dfd13163e5cd82e49827cef3c
Jørgen Loe Kvalberg <j@kvalberg.no>
2024-03-21 13:10:49 +0100

    Koble oppdater-knapp til kode som sjekker om pakke er utdatert.
    Co-authored-by: Øystein Myhre <gruble@users.noreply.github.com>

--- src/app/core/services/offline-map/utils.ts
+++ src/app/core/services/offline-map/utils.ts
@@ -3,7 +3,12 @@ import { OfflineMapPackage } from './offline-map.model';
 
 // Add test
 export function isPackageOutdated(downloadedPackage: OfflineMapPackage, packageOnServer: CompoundPackage) {
-  const downloadedPackageUpdateDate = downloadedPackage.compoundPackageMetadata.getLastModified();
+  const downloadDate = getDownloadCompleteDate(downloadedPackage);
   const packageOnServerUpdateDate = packageOnServer.getLastModified();
-  return downloadedPackageUpdateDate < packageOnServerUpdateDate;
+  return downloadDate < packageOnServerUpdateDate;
+}
+
+export function getDownloadCompleteDate(downloadedPackage: OfflineMapPackage): Date {
+  const timeInMs = downloadedPackage.downloadComplete * 1000;
+  return new Date(timeInMs);
 }
