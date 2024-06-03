575a36ac52094a348d5d638b87a3019377497593
omy <omy@nve.no>
2024-06-03 15:15:24 +0200

    Laget tester og forsøker å løse flere feil med tidssoner

--- src/app/modules/common-core/helpers/date-converters.ts
+++ src/app/modules/common-core/helpers/date-converters.ts
@@ -22,7 +22,18 @@ export function convertToIsoDateTime(date: string, start: 'start' | 'end' = 'sta
  * Eksempel: 2021-09-01T09:00:00+00:00 -> 2021-09-01T11:00:00+02:00
  */
 export function isoDateTimeToLocalDateTimeInIsoFormat(isoDateTimeWithOffset: string): string {
-  const date = new Date(isoDateTimeWithOffset);
+  // if (!isoDateTimeWithOffset) return null;
+  // const utc = new Date(isoDateTimeWithOffset).UTCString();
+  // return new Date(utc).toLocaleString();
+  // const utc = moment(isoDateTimeWithOffset).utc();
+  // return utc.local(false).format('YYYY-MM-DDTHH:mm:ssZ');
+
+  const currentLocalTimeOffsetInMinutes = new Date().getTimezoneOffset();
+  const isoDateTimeInMillis = new Date(isoDateTimeWithOffset).getTime();
+  const isoDateTimeInMillisInLocalTimeZone = isoDateTimeInMillis + currentLocalTimeOffsetInMinutes * 60000;
+  const isoDateTimeInLocalTimeZone = new Date(isoDateTimeInMillisInLocalTimeZone);
+
+  const date = new Date(isoDateTimeInLocalTimeZone);
 
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -35,9 +46,9 @@ export function isoDateTimeToLocalDateTimeInIsoFormat(isoDateTimeWithOffset: str
   const timeZoneOffsetHours = Math.floor(timeZoneOffsetInMinutes / 60);
   const sign = timeZoneOffsetHours > 0 ? '-' : '+';
   const remainingMinutes = Math.abs(timeZoneOffsetInMinutes % 60);
-  const offset = `${Math.abs(timeZoneOffsetHours).toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}`;
+  const offset = `${Math.abs(timeZoneOffsetHours).toString().padStart(2, '0')}:${remainingMinutes
+    .toString()
+    .padStart(2, '0')}`;
 
   return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${sign}${offset}`;
 }
-
-
