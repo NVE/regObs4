ad4421fd036b8843c8c2b9ef66bd358b57bd3862
Jørgen Loe Kvalberg <j@kvalberg.no>
2024-12-17 11:25:49 +0100

    Fjern flere moduler

--- src/app/modules/map/pages/support-map-info/support-map-info.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SupportMapInfoPage } from './support-map-info.page';
-import { SharedModule } from '../../../shared/shared.module';
-
-@NgModule({
-  imports: [SharedModule, SupportMapInfoPage],
-  exports: [SupportMapInfoPage],
-})
-export class SupportMapInfoPageModule {}
