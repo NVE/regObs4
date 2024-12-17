fa943479be27f948cee8d230a642ed673072df0e
Jørgen Loe Kvalberg <j@kvalberg.no>
2024-12-17 11:39:28 +0100

    Fjern enda flere moduler..

--- src/app/modules/registration/components/snow/snow-profile/snow-density/snow-density-layer-modal/snow-density-layer-modal.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SnowDensityLayerModalPage } from './snow-density-layer-modal.page';
-import { SharedComponentsModule } from '../../../../../shared-components.module';
-
-@NgModule({
-  imports: [SharedComponentsModule, SnowDensityLayerModalPage],
-})
-export class SnowDensityLayerModalPageModule {}
--- src/app/modules/registration/components/snow/snow-profile/snow-temp/snow-temp-layer-modal/snow-temp-layer-modal.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SnowTempLayerModalPage } from './snow-temp-layer-modal.page';
-import { SharedComponentsModule } from '../../../../../shared-components.module';
-
-@NgModule({
-  imports: [SharedComponentsModule, SnowTempLayerModalPage],
-})
-export class SnowTempLayerModalPageModule {}
