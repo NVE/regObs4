c14c50fd553ad811e8db054464b0f3d7dad77a0a
Jørgen Loe Kvalberg <j@kvalberg.no>
2025-02-10 13:05:23 +0100

    Fix more tests

--- src/app/modules/side-menu/components/support-tiles-menu/legends/support-tile-legend-loader/support-tile-legend-loader.component.spec.ts
+++ src/app/modules/side-menu/components/support-tiles-menu/legends/support-tile-legend-loader/support-tile-legend-loader.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { SupportTileLegendLoaderComponent } from './support-tile-legend-loader.component';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTestLogger } from 'src/app/modules/shared/services/logging/test-logging.service';
 
 describe('SupportTileLegendLoaderComponent', () => {
   let component: SupportTileLegendLoaderComponent;
@@ -9,11 +10,13 @@ describe('SupportTileLegendLoaderComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [SupportTileLegendLoaderComponent],
+      providers: [provideTranslateService(), provideTestLogger()],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SupportTileLegendLoaderComponent);
+    fixture.componentRef.setInput('name', 'steepness');
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
