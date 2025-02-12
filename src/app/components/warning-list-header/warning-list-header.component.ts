25dab30ca9e72eaf62278852d856c603805f6628
Jørgen Loe Kvalberg <j@kvalberg.no>
2025-02-12 08:55:27 +0100

    Partial fix of warning page
      - Det mangler fortsatt noe på favoritter-siden.
        Empty-state dukker ikke opp.

--- src/app/components/warning-list-header/warning-list-header.component.ts
+++ src/app/components/warning-list-header/warning-list-header.component.ts
@@ -1,25 +1,23 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import moment from 'moment';
 import { IonCol, IonGrid, IonLabel, IonRow, Platform } from '@ionic/angular/standalone';
-import { NgIf, NgClass, NgTemplateOutlet } from '@angular/common';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-warning-list-header',
   templateUrl: './warning-list-header.component.html',
   styleUrls: ['./warning-list-header.component.scss'],
-  imports: [IonCol, IonGrid, IonLabel, IonRow, NgClass, NgIf, NgTemplateOutlet, TranslatePipe],
+  imports: [IonCol, IonGrid, IonLabel, IonRow, NgIf, NgTemplateOutlet, TranslatePipe],
 })
 export class WarningListHeaderComponent {
   private platform = inject(Platform);
 
-  @Input() title: string;
-  @Input() subTitle: string;
-  @Input() showDayNames: boolean;
+  title = input('');
+  subTitle = input('');
+  showDayNames = input(false);
 
-  get ios() {
-    return this.platform.is('ios');
-  }
+  ios = this.platform.is('ios');
 
   getDayName(daysToAdd: number) {
     return `DAYS.SHORT.${moment().add(daysToAdd, 'days').day()}`;
