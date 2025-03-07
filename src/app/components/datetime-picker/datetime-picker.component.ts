28fe068a6b358aa2c5bd796e39d10f7eaab515a6
Jørgen Loe Kvalberg <j@kvalberg.no>
2025-03-07 13:15:13 +0100

    Fix date bug

--- src/app/components/datetime-picker/datetime-picker.component.ts
+++ src/app/components/datetime-picker/datetime-picker.component.ts
@@ -1,151 +1,80 @@
-import {
-  Component,
-  EventEmitter,
-  OnInit,
-  Output,
-  inject,
-  viewChild,
-  input,
-  linkedSignal,
-  model,
-  computed,
-  signal,
-} from '@angular/core';
-import {
-  IonButton,
-  IonButtons,
-  IonContent,
-  IonDatetime,
-  IonHeader,
-  IonModal,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/angular/standalone';
-import { OverlayEventDetail, DatetimePresentation } from '@ionic/core/components';
-import { DatetimeChangeEventDetail } from '@ionic/core/dist/types/components/datetime/datetime-interface';
-import { UserSettingService } from '../../core/services/user-setting/user-setting.service';
-import { firstValueFrom } from 'rxjs';
-import { getLangKeyString } from '../../modules/common-core/models/lang-key.enum';
-import { NgClass, DatePipe } from '@angular/common';
+import { Component, inject, viewChild, input, model, effect, untracked } from '@angular/core';
+import { IonDatetime, IonDatetimeButton, IonModal, Platform } from '@ionic/angular/standalone';
+import { DatetimePresentation } from '@ionic/core/components';
 import { TranslatePipe } from '@ngx-translate/core';
-import { Capacitor } from '@capacitor/core';
+import { FormsModule } from '@angular/forms';
+import moment from 'moment';
+
+// Brukes for å lage unike ider som kobler modal til input
+let counter = 0;
 
 @Component({
   selector: 'app-datetime-picker',
   templateUrl: './datetime-picker.component.html',
   styleUrls: ['./datetime-picker.component.scss'],
-  imports: [
-    DatePipe,
-    IonButton,
-    IonButtons,
-    IonContent,
-    IonDatetime,
-    IonHeader,
-    IonModal,
-    IonTitle,
-    IonToolbar,
-    NgClass,
-    TranslatePipe,
-  ],
+  imports: [IonDatetime, IonModal, TranslatePipe, IonDatetimeButton, FormsModule],
+  host: {
+    '(click)': 'open()',
+  },
 })
 /**
  * Component for displaying a date and time picker.
  * The date and time picker is displayed in a modal, and the selected date and time is returned to the parent component.
  */
-export class DatetimePickerComponent implements OnInit {
-  private userSettings = inject(UserSettingService);
+export class DatetimePickerComponent {
+  private platform = inject(Platform);
+  private modal = viewChild(IonModal);
 
-  readonly dateTime = input<string>(); // Supports Date.prototype.toISOString() format (YYYY-MM-DDTHH:mm:ss.sssZ)
+  readonly dateTime = model<string>(); // Supports Date.prototype.toISOString() format (YYYY-MM-DDTHH:mm:ss.sssZ)
   readonly language = input<string>(); // Automatically sets formatting of Ionic Datetime component. Can be manually overridden.
   readonly minDate = input<string>(); // Sets the min date selectable from the date picker
   readonly maxDate = input<string>(); // Sets the max date selectable from the date picker
-  readonly dateTimeFormat = input('dd. MMM yyyy HH:mm'); // Formats how the dateTime is represented as a string to the user
-  readonly textAlign = input<'left' | 'center' | 'right'>('left');
   readonly presentation = input<DatetimePresentation>('date-time');
-  readonly buttonSize = input<'small' | 'default' | 'large'>('default'); // Sets the main ion-button size (values are from Ionic)
   readonly resetable = input(false);
 
-  // brukes på web til å sette type på input-feltet
-  inputType = computed(() => {
-    switch (this.presentation()) {
-      case 'date':
-        return 'date';
-      case 'time':
-        return 'time';
-    }
-    return 'datetime-local';
+  preferWheel = this.platform.is('mobile');
+
+  dateFormat = input<Pick<Intl.DateTimeFormatOptions, 'day' | 'month' | 'year' | 'weekday'>>({
+    weekday: 'long',
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
   });
-  //  dateTimeLocal = computed(() => (this.dateTime() ? this.dateTime()?.slice(0, 16) : ''));
-  // dateTimeLocal = signal(this.dateTime());
-  dateTimeLocal = signal(new Date().toISOString());
+  timeFormat = input<Pick<Intl.DateTimeFormatOptions, 'hour' | 'minute'>>({
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
-  readonly datePickerOpen = model(false);
+  id: string;
 
-  @Output() dateTimeChange = new EventEmitter<string>(); // Can be used to manually trigger wanted functionality when the dateTime is changed.
+  constructor() {
+    counter++;
+    this.id = 'app-datetime-picker-' + counter;
 
-  locale = linkedSignal(() => this.language());
+    // Fant ikke noe enkel annen måte å oppdatere tiden på hvis max endres.
+    // Det er egentlig ikke anbefalt å oppdatere state fra effect.
+    effect(() => {
+      const maxValue = this.maxDate();
+      if (!maxValue) {
+        return;
+      }
 
-  private tempDate?: string;
+      const dateValue = this.dateTime();
+      if (!dateValue) {
+        return;
+      }
 
-  readonly modal = viewChild(IonModal);
-  readonly isDesktop = !Capacitor.isNativePlatform();
-
-  async ngOnInit(): Promise<void> {
-    if (!this.locale()) {
-      const userSetting = await firstValueFrom(this.userSettings.userSetting$);
-      this.locale.set(getLangKeyString(userSetting.language));
-    }
-    if (this.isDesktop) {
-      // const input = document.querySelector('input');
-      // input?.addEventListener('change', this.updateTempDateTime as EventListener);
-      const input = document.querySelector('input');
-      input?.addEventListener('change', this.updateTempDateTimeFromInput.bind(this) as EventListener);
-    }
+      untracked(() => {
+        const max = moment(maxValue);
+        const date = moment(dateValue);
+        if (date.isValid() && max.isValid() && max.isBefore(date)) {
+          this.dateTime.set(this.maxDate());
+        }
+      });
+    });
   }
 
-  openModal() {
-    this.datePickerOpen.set(true);
-  }
-
-  cancel() {
-    this.modal()?.dismiss(null, 'cancel');
-  }
-
-  confirm() {
-    this.modal()?.dismiss(this.tempDate, 'confirm');
-  }
-
-  /**
-   * If the user clicks the confirm button, emit the date that was selected
-   * @param event - CustomEvent<OverlayEventDetail<string>>
-   */
-  onWillDismiss(event: CustomEvent<OverlayEventDetail<string>>) {
-    this.datePickerOpen.set(false);
-
-    if ((event.detail.data && event.detail.role === 'confirm') || this.resetable()) {
-      this.dateTimeChange.emit(event.detail.data);
-    }
-  }
-
-  /**
-   * Updates the tempDate when the user changes the date, if the event has a value and is not an array
-   * @param event - CustomEvent<DatetimeChangeEventDetail>
-   * @returns false if the event.detail.value is not defined or if it is an array.
-   */
-  updateTempDateTime(event: CustomEvent<DatetimeChangeEventDetail>) {
-    if (!event.detail.value || Array.isArray(event.detail.value)) return;
-    this.tempDate = event.detail.value;
-  }
-
-  /**
-   * Updates the tempDate when the user changes the time using the input element
-   * @param event - Event
-   */
-  updateTempDateTimeFromInput(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.value) {
-      const currentDate = new Date(input.value);
-      this.tempDate = currentDate.toISOString();
-    }
+  open() {
+    this.modal()?.present();
   }
 }
