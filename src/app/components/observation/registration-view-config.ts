eabed24497468506afe270215cc9ef3208c259f5
gruble <omy@nve.no>
2025-03-17 12:51:41 +0100

    Samme sortering av skjema som når vi registrerer en observasjon

--- src/app/components/observation/registration-view-config.ts
+++ src/app/components/observation/registration-view-config.ts
@@ -14,115 +14,137 @@ import { isEmpty } from 'src/app/modules/common-core/helpers';
  * Et alternativ hadde vært å feks falle tilbake på en Summary-baserte visning hvis
  * ikke det fantes en konfigurasjon for den registreringstypen.
  */
-export const REGISTRATION_VIEW_CONFIG: Record<number, RegistrationViewConfig> = {
-  [RegistrationTid.GeneralObservation]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.GeneralObservation),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.GeneralObservation),
-  },
-  [RegistrationTid.Incident]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.Incident),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.Incident),
-  },
-  [RegistrationTid.DangerObs]: {
+export const REGISTRATION_VIEW_CONFIG: RegistrationViewConfig[] = [
+  {
+    tid: RegistrationTid.DangerObs,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.DangerObs),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.DangerObs),
   },
-  [RegistrationTid.DamageObs]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.DamageObs),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.DamageObs),
-  },
-  [RegistrationTid.WeatherObservation]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.WeatherObservation),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.WeatherObservation),
-  },
-  [RegistrationTid.SnowSurfaceObservation]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.SnowSurfaceObservation),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.SnowSurfaceObservation),
-  },
-  [RegistrationTid.SnowCoverObs]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.SnowCoverObs),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.SnowCoverObs),
-  },
-  [RegistrationTid.AvalancheDangerObs]: {
+  {
+    tid: RegistrationTid.AvalancheDangerObs,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheDangerObs),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheDangerObs),
   },
-  [RegistrationTid.CompressionTest]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.CompressionTest),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.CompressionTest),
-  },
-  [RegistrationTid.AvalancheObs]: {
+  {
+    tid: RegistrationTid.AvalancheObs,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheObs),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheObs),
   },
-  [RegistrationTid.AvalancheActivityObs]: {
+  {
+    tid: RegistrationTid.Incident,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.Incident),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.Incident),
+  },
+  {
+    tid: RegistrationTid.DamageObs,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.DamageObs),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.DamageObs),
+  },
+  {
+    tid: RegistrationTid.AvalancheActivityObs,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheActivityObs),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheActivityObs),
   },
-  [RegistrationTid.AvalancheEvaluation]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheEvaluation),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheEvaluation),
-  },
-  [RegistrationTid.AvalancheEvaluation2]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheEvaluation2),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheEvaluation2),
-  },
-  [RegistrationTid.AvalancheEvaluation3]: {
-    component: AvalancheEvaluationViewComponent,
-    isEmpty: (reg) => isEmpty(reg.AvalancheEvaluation3),
-    getInputs: (reg) => ({ data: reg.AvalancheEvaluation3 }),
-  },
-  [RegistrationTid.AvalancheEvalProblem2]: {
-    component: AbalancheProblemsViewComponent,
-    isEmpty: (reg) => isEmpty(reg.AvalancheEvalProblem2),
-    getInputs: (reg) => ({ data: reg.AvalancheEvalProblem2 }),
-  },
-  [RegistrationTid.AvalancheActivityObs2]: {
+  {
+    tid: RegistrationTid.AvalancheActivityObs2,
     component: AvalancheActivitesViewComponent,
     isEmpty: (reg) => isEmpty(reg.AvalancheActivityObs2),
     getInputs: (reg) => ({ data: reg.AvalancheActivityObs2 }),
   },
-  [RegistrationTid.SnowProfile2]: {
+  {
+    tid: RegistrationTid.WeatherObservation,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.WeatherObservation),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.WeatherObservation),
+  },
+  {
+    tid: RegistrationTid.SnowSurfaceObservation,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.SnowSurfaceObservation),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.SnowSurfaceObservation),
+  },
+  {
+    tid: RegistrationTid.SnowCoverObs,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.SnowCoverObs),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.SnowCoverObs),
+  },
+  {
+    tid: RegistrationTid.CompressionTest,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.CompressionTest),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.CompressionTest),
+  },
+  {
+    tid: RegistrationTid.SnowProfile2,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.SnowProfile2),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.SnowProfile2),
   },
-  [RegistrationTid.IceThickness]: {
-    component: SummaryComponent,
-    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.IceThickness),
-    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.IceThickness),
+  {
+    tid: RegistrationTid.AvalancheEvalProblem2,
+    component: AbalancheProblemsViewComponent,
+    isEmpty: (reg) => isEmpty(reg.AvalancheEvalProblem2),
+    getInputs: (reg) => ({ data: reg.AvalancheEvalProblem2 }),
   },
-  [RegistrationTid.IceCoverObs]: {
+  {
+    tid: RegistrationTid.AvalancheEvaluation,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheEvaluation),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheEvaluation),
+  },
+  {
+    tid: RegistrationTid.AvalancheEvaluation2,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.AvalancheEvaluation2),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.AvalancheEvaluation2),
+  },
+  {
+    tid: RegistrationTid.AvalancheEvaluation3,
+    component: AvalancheEvaluationViewComponent,
+    isEmpty: (reg) => isEmpty(reg.AvalancheEvaluation3),
+    getInputs: (reg) => ({ data: reg.AvalancheEvaluation3 }),
+  },
+  {
+    tid: RegistrationTid.IceCoverObs,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.IceCoverObs),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.IceCoverObs),
   },
-  [RegistrationTid.WaterLevel]: {
+  {
+    tid: RegistrationTid.IceThickness,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.IceThickness),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.IceThickness),
+  },
+  {
+    tid: RegistrationTid.WaterLevel,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.WaterLevel),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.WaterLevel),
   },
-  [RegistrationTid.WaterLevel2]: {
+  {
+    tid: RegistrationTid.WaterLevel2,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.WaterLevel2),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.WaterLevel2),
   },
-  [RegistrationTid.LandSlideObs]: {
+  {
+    tid: RegistrationTid.LandSlideObs,
     component: SummaryComponent,
     isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.LandSlideObs),
     getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.LandSlideObs),
   },
-};
+  {
+    tid: RegistrationTid.GeneralObservation,
+    component: SummaryComponent,
+    isEmpty: (reg) => isObservationModelEmptyForRegistrationTid(reg, RegistrationTid.GeneralObservation),
+    getInputs: (reg) => getSummaryInputs(reg, RegistrationTid.GeneralObservation),
+  },
+];
