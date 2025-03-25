52a599e24344a5de342fa8e78716c710c16fc7bc
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2025-03-25 11:10:14 +0100

    RO-1345: legge til himmelretning og opphavsrett under bilder (#703)
    * legg til himmelretning og opphavsrett under bilder
    
    ads
    
    gfd
    
    * comments
    
    edit after comments
    
    comments #3
    
    * comments #2

--- src/app/modules/edit-user-nickname-modal/edit-user-nickname-modal.component.ts
+++ src/app/modules/edit-user-nickname-modal/edit-user-nickname-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, model, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonIcon, IonInput, IonItem, IonList, ModalController, IonText } from '@ionic/angular/standalone';
+import { IonButton, IonIcon, IonInput, IonItem, IonList, ModalController } from '@ionic/angular/standalone';
 import { TranslatePipe } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
 import { close } from 'ionicons/icons';
@@ -9,7 +9,7 @@ import { close } from 'ionicons/icons';
   selector: 'app-edit-user-nickname-modal',
   templateUrl: './edit-user-nickname-modal.component.html',
   styleUrls: ['./edit-user-nickname-modal.component.scss'],
-  imports: [IonText, FormsModule, IonButton, IonIcon, IonInput, IonItem, IonList, TranslatePipe],
+  imports: [FormsModule, IonButton, IonIcon, IonInput, IonItem, IonList, TranslatePipe],
 })
 export class EditUserNicknameModalComponent {
   modalController = inject(ModalController);
