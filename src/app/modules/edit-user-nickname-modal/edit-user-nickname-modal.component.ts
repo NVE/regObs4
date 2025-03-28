fabd95f0148950f35697d8693b02722b5696d95e
omy <omy@nve.no>
2025-03-28 13:57:59 +0100

    Gjort utformering mer i tråd med Ionic retningslinjer for modal

--- src/app/modules/edit-user-nickname-modal/edit-user-nickname-modal.component.ts
+++ src/app/modules/edit-user-nickname-modal/edit-user-nickname-modal.component.ts
@@ -1,6 +1,17 @@
 import { Component, inject, model, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonIcon, IonInput, IonItem, IonList, ModalController } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonInput,
+  IonItem,
+  IonList,
+  ModalController,
+  IonContent,
+  IonButtons,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 import { TranslatePipe } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
 import { close } from 'ionicons/icons';
@@ -9,7 +20,19 @@ import { close } from 'ionicons/icons';
   selector: 'app-edit-user-nickname-modal',
   templateUrl: './edit-user-nickname-modal.component.html',
   styleUrls: ['./edit-user-nickname-modal.component.scss'],
-  imports: [FormsModule, IonButton, IonIcon, IonInput, IonItem, IonList, TranslatePipe],
+  imports: [
+    IonHeader,
+    IonButtons,
+    IonContent,
+    FormsModule,
+    IonButton,
+    IonInput,
+    IonItem,
+    IonList,
+    IonToolbar,
+    IonTitle,
+    TranslatePipe,
+  ],
 })
 export class EditUserNicknameModalComponent {
   modalController = inject(ModalController);
