6597ac4a5a005a30e8b5847147b05379b0612073
marcin <martin.joodd@gmail.com>
2025-03-31 14:18:53 +0200

    addons

--- src/app/modules/login/pages/user-information/user-information.page.ts
+++ src/app/modules/login/pages/user-information/user-information.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, computed } from '@angular/core';
 import { firstValueFrom, Observable, of } from 'rxjs';
 import { LoggedInUser } from '../../models/logged-in-user.model';
 import { RegobsAuthService } from '../../../auth/services/regobs-auth.service';
