5dc77a40d110da8a05df7e9326de73d016e31906
marcin <martin.joodd@gmail.com>
2025-04-01 16:27:26 +0200

    fix image list
    asd
    
    set images back to grid
    
    adjust width of the img
    
    last cleaniong

--- src/app/pages/observation-list/image-list/grid-image.component.ts
+++ src/app/pages/observation-list/image-list/grid-image.component.ts
@@ -10,15 +10,39 @@ import { AttachmentViewModel } from 'src/app/modules/common-regobs-api';
   selector: 'app-grid-image',
   imports: [],
   template: `
-    <img [src]="src()" (load)="setAspectRatioClass($event)" />
-    <div>{{ attachment().RegistrationName }}</div>
-    <div>
-      <i>{{ attachment().Comment }}</i>
+    <div class="grid-image">
+      <img [src]="src()" (load)="setAspectRatioClass($event)" />
+
+      @if (attachment().RegistrationName) {
+        <div class="grid-image__text">{{ attachment().RegistrationName }}</div>
+      }
+      @if (attachment().Comment) {
+        <div class="grid-image__text">
+          <i>{{ attachment().Comment }}</i>
+        </div>
+      }
     </div>
   `,
   styles: `
-    :host {
-      display: block;
+    .grid-image {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      padding: 10px;
+      box-sizing: border-box;
+    }
+    .grid-image__text {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      line-height: 100%;
+    }
+    img {
+      flex: 1;
+      object-fit: cover;
+      object-position: right center;
+      margin-bottom: 7px;
     }
   `,
   host: {
