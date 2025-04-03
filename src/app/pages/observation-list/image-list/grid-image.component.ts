02b6566237148d9f2b1c5bd3f33a81b6b5f16526
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2025-04-03 15:31:31 +0200

    RO-2771: Fikse beskrivelser i bildevisning (#728)

--- src/app/pages/observation-list/image-list/grid-image.component.ts
+++ src/app/pages/observation-list/image-list/grid-image.component.ts
@@ -10,15 +10,42 @@ import { AttachmentViewModel } from 'src/app/modules/common-regobs-api';
   selector: 'app-grid-image',
   imports: [],
   template: `
-    <img [src]="src()" (load)="setAspectRatioClass($event)" />
-    <div>{{ attachment().RegistrationName }}</div>
-    <div>
-      <i>{{ attachment().Comment }}</i>
-    </div>
+    <img [src]="src()" (load)="setAspectRatioClass($event)" [alt]="attachmentAlt()" />
+
+    @if (attachment().RegistrationName) {
+      <div class="grid-image__text">{{ attachment().RegistrationName }}</div>
+    }
+    @if (attachment().Comment) {
+      <div class="grid-image__text">
+        <i>{{ attachment().Comment }}</i>
+      </div>
+    }
   `,
   styles: `
     :host {
-      display: block;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      padding: 10px;
+      box-sizing: border-box;
+      background: #fff;
+    }
+    .grid-image__text {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      line-height: 100%;
+    }
+
+    .grid-image__text + .grid-image__text {
+      margin-top: 4px;
+    }
+
+    img {
+      flex: 1;
+      object-fit: cover;
+      object-position: right center;
+      margin-bottom: 7px;
     }
   `,
   host: {
@@ -31,6 +58,10 @@ export class GridImageComponent {
 
   hostClass = signal<string>('');
   src = computed(() => this.attachment().UrlFormats?.Large);
+
+  attachmentAlt = computed(
+    () => `${this.attachment().RegistrationName} ${this.attachment().Comment ? ' - ' + this.attachment().Comment : ''}`
+  );
 
   setAspectRatioClass($event: Event) {
     const img = $event.target as HTMLImageElement;
