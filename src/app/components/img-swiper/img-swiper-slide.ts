0bf833ebc3863d0c80733be8f7efff09f7efcd34
Jørgen Loe Kvalberg <jolokv@nve.no>
2025-04-08 11:36:35 +0200

    Remove old image swiper

--- src/app/components/img-swiper/img-swiper-slide.ts
+++
@@ -1,9 +0,0 @@
-import { AttachmentViewModel } from 'src/app/modules/common-regobs-api';
-import { ImageLocation } from './image-location.model';
-
-export interface ImgSwiperSlide {
-  type: 'location' | 'image';
-  img: AttachmentViewModel | ImageLocation;
-  header?: string;
-  description?: string;
-}
