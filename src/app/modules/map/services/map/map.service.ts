10731c1aebc6237c457af40d0f84cf58b6091c66
Jørgen Loe Kvalberg <jolokv@nve.no>
2025-04-29 09:34:18 +0200

    RO-2803: Sett bounds for kartet til svalbard+norway hvis ikke annet er gitt (#761)

--- src/app/modules/map/services/map/map.service.ts
+++ src/app/modules/map/services/map/map.service.ts
@@ -29,6 +29,7 @@ import {
   URL_PARAM_SE_LON,
 } from 'src/app/core/services/search-criteria/url-params';
 import { GeoHazard } from 'src/app/modules/common-core/models';
+import { settings } from 'src/settings';
 
 type WithMargin = (ob: L.LatLngBoundsExpression, maxMargin: number) => boolean;
 
@@ -40,7 +41,7 @@ export const createMapView = (nwLat: number, nwLon: number, seLat: number, seLon
   return mapView;
 };
 
-export const parseCoordinatesFromSearchParams = (params: URLSearchParams): IMapView | undefined => {
+export const parseMapViewFromSearchParams = (params: URLSearchParams): IMapView | undefined => {
   const nwLat = params.get(URL_PARAM_NW_LAT);
   const nwLon = params.get(URL_PARAM_NW_LON);
   const seLat = params.get(URL_PARAM_SE_LAT);
@@ -50,6 +51,17 @@ export const parseCoordinatesFromSearchParams = (params: URLSearchParams): IMapV
     return formatedMapView;
   }
   return;
+};
+
+const getInitialMapView = (): IMapView => {
+  const mapView = parseMapViewFromSearchParams(getSearchParams());
+  if (mapView) return mapView;
+
+  const bounds = L.latLngBounds([settings.map.startupBounds.topLeft, settings.map.startupBounds.bottomRight]);
+  return {
+    bounds,
+    center: bounds.getCenter(),
+  };
 };
 
 const getSearchParams = () => {
@@ -75,9 +87,7 @@ export class MapService {
   private _showUserLocationObservable: Observable<boolean>;
   private _centerMapToUserSubject: Subject<void>;
   private _centerMapToUserObservable: Observable<void>;
-  private _mapViewSubject = new BehaviorSubject<IMapView | undefined>(
-    parseCoordinatesFromSearchParams(getSearchParams())
-  );
+  private _mapViewSubject = new BehaviorSubject<IMapView>(getInitialMapView());
   private _mapView$: Observable<IMapView | undefined>;
   private _noMapExtentAvailable$: Observable<boolean>;
   private _relevantMapChange$: Observable<IMapView>;
