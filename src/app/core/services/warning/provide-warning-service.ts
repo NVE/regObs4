93926c290f0ab1185fdc818aa55fecc8eb2ba629
marcin <martin.joodd@gmail.com>
2025-05-13 13:29:36 +0200

    revert

--- src/app/core/services/warning/provide-warning-service.ts
+++ src/app/core/services/warning/provide-warning-service.ts
@@ -5,11 +5,24 @@ import { inject } from '@angular/core';
 import { LoggingService } from 'src/app/modules/shared/services/logging/logging.service';
 
 export function provideWarningService() {
-  if (true) {
+  if (Capacitor.isNativePlatform()) {
     return {
       provide: WarningService,
       useClass: WarningService,
     };
   } else {
+    return {
+      provide: WarningService,
+      useFactory: () => {
+        const logger = inject(LoggingService);
+        const tag = 'WarningService';
+        return {
+          warningGroupInMapViewObservable$: of(null).pipe(
+            tap(() => logger.debug('warningGroupInMapViewObservable$ subsribed but doing nothing on web', tag))
+          ),
+          updateWarnings: () => logger.debug('updateWarnings called but doing nothing on web', tag),
+        };
+      },
+    };
   }
 }
