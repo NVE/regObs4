6e2dd11d63e7fa1e0ddf21c5fe5a83301b4a1f17
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2025-05-30 10:14:37 +0200

    RO-2899: fikse utsatt høyde visning i skredaktivitet skjema (#812)
    Co-authored-by: gruble <omy@nve.no>

--- src/app/modules/registration/components/snow/exposed-height/exposed-height.component.ts
+++ src/app/modules/registration/components/snow/exposed-height/exposed-height.component.ts
@@ -1,9 +1,8 @@
-import { IonGrid, IonItem, IonRow, IonCol, IonText, IonLabel } from '@ionic/angular/standalone';
-import { Component, model, computed } from '@angular/core';
-import { SelectOption } from '../../../../shared/components/input/select/select-option.model';
-import { NgClass } from '@angular/common';
-import { SelectComponent } from '../../../../shared/components/input/select/select.component';
+import { Component, computed, model, signal } from '@angular/core';
+import { IonCol, IonGrid, IonItem, IonLabel, IonRow, IonText } from '@ionic/angular/standalone';
 import { TranslatePipe } from '@ngx-translate/core';
+import { SelectOption } from '../../../../shared/components/input/select/select-option.model';
+import { SelectComponent } from '../../../../shared/components/input/select/select.component';
 
 interface HeightSelectOption extends SelectOption {
   id: number;
@@ -17,20 +16,34 @@ enum ExposedHeightCombo {
   MiddleBlack = 4,
 }
 
+/** Fjell høyde områder */
+type ExposedHeightPosition = 'top' | 'middle' | 'bottom';
+
+/** Nøkkel er fjell utsatt høyde, verdi er om den er valgt.  */
+type HeightsState = Record<ExposedHeightPosition, boolean>;
+
 @Component({
   selector: 'app-exposed-height',
   templateUrl: './exposed-height.component.html',
   styleUrls: ['./exposed-height.component.scss'],
-  imports: [IonCol, IonGrid, IonItem, IonLabel, IonRow, IonText, NgClass, SelectComponent, TranslatePipe],
+  imports: [IonCol, IonGrid, IonItem, IonLabel, IonRow, IonText, SelectComponent, TranslatePipe],
 })
 export class ExposedHeightComponent {
   readonly exposedHeightComboTID = model<number>();
   readonly exposedHeight1 = model<number>();
   readonly exposedHeight2 = model<number>();
 
-  exposedHeightTop = false;
-  exposedHeightMiddle = false;
-  exposedHeightBottom = false;
+  // Valgte høyder
+  heights = signal<HeightsState>({
+    top: false,
+    middle: false,
+    bottom: false,
+  });
+
+  // Høyder gjort om til et array for enklere iterasjon i html malen
+  heightEntries = computed(() =>
+    Object.entries(this.heights()).map(([key, value]) => ({ key: key as ExposedHeightPosition, value }))
+  );
 
   heightArray = createHeightArray();
 
@@ -40,80 +53,59 @@ export class ExposedHeightComponent {
   });
 
   ngOnInit() {
-    this.setExposedHeights(this.exposedHeightComboTID());
-  }
-
-  setExposedHeights(exposedHeightComboTID: number | undefined) {
-    if (exposedHeightComboTID === 0) {
-      this.exposedHeightTop = true;
-      this.exposedHeightMiddle = true;
-      this.exposedHeightBottom = true;
-    } else if (exposedHeightComboTID === 1) {
+    if (this.exposedHeightComboTID() === 0) {
+      this.heights.update(() => ({ top: true, middle: true, bottom: true }));
+    } else if (this.exposedHeightComboTID() === 1) {
       // Hvit nederst
-      this.exposedHeightTop = true;
-      this.exposedHeightMiddle = true;
-      this.exposedHeightBottom = false;
-    } else if (exposedHeightComboTID === 2) {
+      this.heights.update((heights) => ({ ...heights, top: true, bottom: true }));
+    } else if (this.exposedHeightComboTID() === 2) {
       // Svart nederst
-      this.exposedHeightTop = false;
-      this.exposedHeightMiddle = false;
-      this.exposedHeightBottom = true;
-    } else if (exposedHeightComboTID === 3) {
+      this.heights.update((heights) => ({ ...heights, bottom: true }));
+    } else if (this.exposedHeightComboTID() === 3) {
       // Hvit i midten
-      this.exposedHeightTop = true;
-      this.exposedHeightMiddle = false;
-      this.exposedHeightBottom = true;
-    } else if (exposedHeightComboTID === 4) {
+      this.heights.update((heights) => ({ ...heights, top: true, bottom: true }));
+    } else if (this.exposedHeightComboTID() === 4) {
       // Svart i midten
-      this.exposedHeightTop = false;
-      this.exposedHeightMiddle = true;
-      this.exposedHeightBottom = false;
-    } else {
-      this.exposedHeightTop = false;
-      this.exposedHeightMiddle = false;
-      this.exposedHeightBottom = false;
+      this.heights.update((heights) => ({ ...heights, middle: true }));
     }
   }
 
   toggleExsposedHeightCombo(position: 'top' | 'middle' | 'bottom') {
-    if (position === 'top') {
-      this.exposedHeightTop = !this.exposedHeightTop;
-    } else if (position === 'middle') {
-      this.exposedHeightMiddle = !this.exposedHeightMiddle;
-    } else {
-      this.exposedHeightBottom = !this.exposedHeightBottom;
-    }
+    this.heights.update((heights) => ({ ...heights, [position]: !heights[position] }));
     this.applyChanges();
   }
 
-  sholdUseExposedHight2() {
-    return (
-      (this.exposedHeightTop && this.exposedHeightBottom && !this.exposedHeightMiddle) ||
-      (!this.exposedHeightTop && !this.exposedHeightBottom && this.exposedHeightMiddle)
-    );
-  }
-
-  private updateExposedHeightComboTID(top: boolean, middle: boolean, bottom: boolean) {
-    if (top && middle && bottom) {
-      this.exposedHeightComboTID.set(ExposedHeightCombo.NotGiven);
-    } else if (!top && middle && !bottom) {
-      this.exposedHeightComboTID.set(ExposedHeightCombo.MiddleBlack);
-    } else if (top && !middle && bottom) {
-      this.exposedHeightComboTID.set(ExposedHeightCombo.MiddleWhite);
-    } else if (bottom) {
-      this.exposedHeightComboTID.set(ExposedHeightCombo.BottomBlack);
-    } else if (top) {
-      this.exposedHeightComboTID.set(ExposedHeightCombo.BottomWhite);
-    } else {
-      this.exposedHeightComboTID.set(undefined);
-    }
-  }
+  sholdUseExposedHight2 = computed(
+    () =>
+      (this.heights().top && this.heights().bottom && !this.heights().middle) ||
+      (!this.heights().top && !this.heights().bottom && this.heights().middle)
+  );
 
   applyChanges() {
-    this.updateExposedHeightComboTID(this.exposedHeightTop, this.exposedHeightMiddle, this.exposedHeightBottom);
     if (!this.sholdUseExposedHight2()) {
       this.exposedHeight2.set(undefined);
     }
+    const currentExposedHeightComboTID = this.currentExposedHeightComboTID();
+    this.exposedHeightComboTID.set(currentExposedHeightComboTID);
+  }
+
+  private currentExposedHeightComboTID(): number | undefined {
+    if (this.heights().top && this.heights().middle && this.heights().bottom) {
+      return ExposedHeightCombo.NotGiven;
+    }
+    if (!this.heights().top && this.heights().middle && !this.heights().bottom) {
+      return ExposedHeightCombo.MiddleBlack;
+    }
+    if (this.heights().top && !this.heights().middle && this.heights().bottom) {
+      return ExposedHeightCombo.MiddleWhite;
+    }
+    if (this.heights().bottom) {
+      return ExposedHeightCombo.BottomBlack;
+    }
+    if (this.heights().top) {
+      return ExposedHeightCombo.BottomWhite;
+    }
+    return undefined;
   }
 }
 
