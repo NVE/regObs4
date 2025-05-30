7246278ed6a0a273f3eac981cb75da2ba7f02823
marcin <martin.joodd@gmail.com>
2025-05-30 12:36:27 +0200

    fikse fitlermeny tittel

--- src/app/modules/side-menu/components/filter-menu/filter-menu.component.ts
+++ src/app/modules/side-menu/components/filter-menu/filter-menu.component.ts
@@ -14,6 +14,7 @@ import {
   SearchbarCustomEvent,
   IonContent,
   IonListHeader,
+  Platform,
 } from '@ionic/angular/standalone';
 import { ChangeDetectionStrategy, Component, OnInit, Signal, computed, inject, signal } from '@angular/core';
 import { distinctUntilChanged, map } from 'rxjs/operators';
@@ -87,9 +88,10 @@ export class FilterMenuComponent extends NgDestoryBase implements OnInit {
   private searchCriteriaService = inject(SearchCriteriaService);
   private searchCriteriaModelService = inject(SearchCriteriaModelService);
   private kdvService = inject(KdvService);
+  private platform = inject(Platform);
   private slushFlowKdv = toSignal(this.kdvService.getKdvRepositoryByKeyObservable('Snow_AvalancheKDV'));
   observationTypeGroups = toSignal(this.searchCriteriaModelService.getObservationTypeGroups$());
-
+  isMobile = this.platform.is('android') || this.platform.is('ios') || this.platform.is('mobileweb');
   private currentGeoHazard = toSignal(this.userSettingService.currentGeoHazard$, { initialValue: [GeoHazard.Snow] });
   isGeohazardSnow = computed(() => this.currentGeoHazard().includes(GeoHazard.Snow));
 
