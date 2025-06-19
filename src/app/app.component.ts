bce4e999217ee3dbfbca9c40505dd0f8802feccd
Jørgen Loe Kvalberg <j@kvalberg.no>
2025-06-19 13:57:03 +0200

    Endre fra service til funksjon for å ha kontroll på async kall

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { SplashScreen } from '@capacitor/splash-screen';
 import { UserSettingService } from './core/services/user-setting/user-setting.service';
 import { DataMarshallService } from './core/services/data-marshall/data-marshall.service';
 import { SwipeBackService } from './core/services/swipe-back/swipe-back.service';
-import { Observable, firstValueFrom } from 'rxjs';
+import { Observable, concatMap, firstValueFrom, tap } from 'rxjs';
 import { LoggingService } from './modules/shared/services/logging/logging.service';
 import { DbHelperService } from './core/services/db-helper/db-helper.service';
 import { ShortcutService } from './core/services/shortcut/shortcut.service';
@@ -20,7 +20,7 @@ import { AsyncPipe } from '@angular/common';
 import { GpsDebugComponent } from './modules/gps-debug/components/gps-debug/gps-debug.component';
 import { RouterLink } from '@angular/router';
 import { Capacitor } from '@capacitor/core';
-import { StatusbarColorService } from './core/services/statusbarColor.service';
+import { setBackgroundColor } from './core/services/statusbarColor.service';
 
 const DEBUG_TAG = 'AppComponent';
 
@@ -52,7 +52,6 @@ const DEBUG_TAG = 'AppComponent';
   `,
 })
 export class AppComponent {
-  private statusbarColorService = inject(StatusbarColorService); // brukes ikke her, men må være her for å startes
   private platform = inject(Platform);
   private userSettings = inject(UserSettingService);
   private dataMarshallService = inject(DataMarshallService);
@@ -90,6 +89,17 @@ export class AppComponent {
     this.logIfError(result);
 
     this.breakpointService.onResize(this.platform.width());
+
+    // Fargen på menylinja endres når appMode endres.
+    // Se docstring på setBackgroundColor
+    if (Capacitor.isNativePlatform()) {
+      this.userSettings.appMode$
+        .pipe(
+          concatMap((appMode) => setBackgroundColor(appMode)),
+          tap((appMode) => this.loggingService.debug('Changed statusbar color', DEBUG_TAG, { appMode }))
+        )
+        .subscribe();
+    }
 
     this.afterAppInitialized();
   }
--- src/app/core/services/statusbarColor.service.ts
+++ src/app/core/services/statusbarColor.service.ts
@@ -1,32 +1,56 @@
-import { UserSettingService } from './user-setting/user-setting.service';
-import { inject, Injectable } from '@angular/core';
-import { Capacitor } from '@capacitor/core';
+// import { UserSettingService } from './user-setting/user-setting.service';
+// import { inject, Injectable } from '@angular/core';
+// import { Capacitor } from '@capacitor/core';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { getHeaderThemeColor as getHeaderThemeColor } from 'src/app/utils/color-utils';
+// import { EdgeToEdge } from '@capawesome/capacitor-android-edge-to-edge-support';
+import { AppMode } from 'src/app/modules/common-core/models';
 
 /**
  * Setter bakgrunnsfarge på statuslinja i OS'et til det samme som menylinja øverst
- * Fargen på menylinja endres når appMode endres.
+ *
+ * NB: Det virker som farger definert i android/app/src/main/res/values/styles.xml også har en effekt,
+ * i alle fall inntil setBackgroundColor kalles første gang.
+ *
+ * Det virker også som setBackgroundColor ikke har noen effekt om den kalles for tidlig under oppstarten.
  */
-@Injectable({
-  providedIn: 'root',
-})
-export class StatusbarColorService {
-  private userSettingService = inject(UserSettingService);
+export async function setBackgroundColor(appMode: AppMode = AppMode.Prod) {
+  const themeColor = getHeaderThemeColor(appMode);
 
-  constructor() {
-    if (Capacitor.isNativePlatform()) {
-      // Setter bakgrunnsfarge på statuslinja i OS'et til det samme som "menylinja" øverst
-      this.userSettingService.appMode$.subscribe((appMode) => {
-        const themeColor = getHeaderThemeColor(appMode);
+  // Henter hex-verdien for fargen fra CSS
+  const documentStyle = getComputedStyle(document.body);
+  const colorValue = documentStyle.getPropertyValue(`--ion-color-${themeColor}`);
 
-        // Henter hex-verdien for fargen fra CSS
-        const documentStyle = getComputedStyle(document.body);
-        const colorValue = documentStyle.getPropertyValue(`--ion-color-${themeColor}`);
-
-        StatusBar.setBackgroundColor({ color: colorValue });
-        StatusBar.setStyle({ style: Style.Dark }); // Lys tekst
-      });
-    }
-  }
+  // await EdgeToEdge.enable();
+  // await EdgeToEdge.setBackgroundColor({ color: '#c0ffee' });
+  await StatusBar.setBackgroundColor({ color: colorValue });
+  await StatusBar.setStyle({ style: Style.Dark }); // Lys tekst
 }
+
+// /**
+//  * Setter bakgrunnsfarge på statuslinja i OS'et til det samme som menylinja øverst
+//  * Fargen på menylinja endres når appMode endres.
+//  */
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class StatusbarColorService {
+//   private userSettingService = inject(UserSettingService);
+
+//   constructor() {
+//     if (Capacitor.isNativePlatform()) {
+//       // Setter bakgrunnsfarge på statuslinja i OS'et til det samme som "menylinja" øverst
+//       this.userSettingService.appMode$.subscribe((appMode) => {
+//         const themeColor = getHeaderThemeColor(appMode);
+
+//         // Henter hex-verdien for fargen fra CSS
+//         const documentStyle = getComputedStyle(document.body);
+//         const colorValue = documentStyle.getPropertyValue(`--ion-color-${themeColor}`);
+
+//         EdgeToEdge.setBackgroundColor({ color: '#ffffff' });
+//         StatusBar.setBackgroundColor({ color: colorValue });
+//         StatusBar.setStyle({ style: Style.Dark }); // Lys tekst
+//       });
+//     }
+//   }
+// }
