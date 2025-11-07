018a952c194cb0747b3e03c3b7199cbb6e2e6faf
Jørgen Loe Kvalberg <jolokv@nve.no>
2025-11-07 15:57:43 +0100

    Legg til filimport med filedrop

--- src/app/pages/plans/plans.page.ts
+++ src/app/pages/plans/plans.page.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from 'src/app/modules/shared/components/header/header
 import 'nve-designsystem/components/nve-button/nve-button.component.js';
 import 'nve-designsystem/components/nve-icon/nve-icon.component.js';
 import 'nve-designsystem/components/nve-message-card/nve-message-card.component.js';
+import { NgxFileDropEntry, NgxFileDropModule } from 'ngx-file-drop';
 
 @Component({
   selector: 'app-plans',
@@ -12,7 +13,7 @@ import 'nve-designsystem/components/nve-message-card/nve-message-card.component.
   styleUrl: './plans.page.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [HeaderComponent, IonButtons, IonMenuButton, IonTitle, TranslatePipe, TranslatePipe],
+  imports: [HeaderComponent, IonButtons, IonMenuButton, IonTitle, TranslatePipe, TranslatePipe, NgxFileDropModule],
 })
 /** Side som viser planer og sporfiler */
 export class PlansPage {
@@ -25,4 +26,12 @@ export class PlansPage {
       this.showImportmessage.set(false);
     }, 3000);
   };
+  /**
+   * Log GPX filenames when files are dropped in the dropzone
+   */
+  onGpxDrop(gpxFiles: NgxFileDropEntry[]) {
+    for (const gpxFile of gpxFiles) {
+      console.log(gpxFile.relativePath);
+    }
+  }
 }
