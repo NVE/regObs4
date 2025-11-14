9cc3763725ae69ca6fc4a55dda2db477a7f1ad42
Jørgen Loe Kvalberg <jolokv@nve.no>
2025-11-14 14:09:25 +0100

    RO-3033: listevisning av turer (#869)
    Co-authored-by: marcin <martin.joodd@gmail.com>

--- src/app/pages/plans/plans.page.ts
+++ src/app/pages/plans/plans.page.ts
@@ -1,9 +1,15 @@
 import { Platform } from '@ionic/angular';
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, CUSTOM_ELEMENTS_SCHEMA, inject, signal } from '@angular/core';
 import { IonButtons, IonMenuButton, IonTitle } from '@ionic/angular/standalone';
 import { TranslatePipe } from '@ngx-translate/core';
 import { HeaderComponent } from 'src/app/modules/shared/components/header/header.component';
 import 'nve-designsystem/components/nve-button/nve-button.component.js';
+import 'nve-designsystem/components/nve-menu/nve-menu.component.js';
+import 'nve-designsystem/components/nve-menu-item/nve-menu-item.component.js';
+import 'nve-designsystem/components/nve-divider/nve-divider.component.js';
+import 'nve-designsystem/components/nve-badge/nve-badge.component.js';
+import 'nve-designsystem/components/nve-select/nve-select.component.js';
+import 'nve-designsystem/components/nve-option/nve-option.component.js';
 import 'nve-designsystem/components/nve-icon/nve-icon.component.js';
 import 'nve-designsystem/components/nve-message-card/nve-message-card.component.js';
 import { NgxFileDropEntry, NgxFileDropModule } from 'ngx-file-drop';
@@ -11,6 +17,7 @@ import { toGeoJSON } from './gpx';
 import { GeoJSONService } from 'src/app/core/services/geojson/geojson.service';
 import { GeoJSONItem } from 'src/app/core/services/geojson/geojson-item.model';
 import { generateShortRandomId } from './utils';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-plans',
@@ -18,7 +25,16 @@ import { generateShortRandomId } from './utils';
   styleUrl: './plans.page.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [HeaderComponent, IonButtons, IonMenuButton, IonTitle, TranslatePipe, TranslatePipe, NgxFileDropModule],
+  imports: [
+    HeaderComponent,
+    IonButtons,
+    DatePipe,
+    IonMenuButton,
+    IonTitle,
+    TranslatePipe,
+    TranslatePipe,
+    NgxFileDropModule,
+  ],
 })
 /** Side som viser planer og sporfiler */
 export class PlansPage {
@@ -26,7 +42,12 @@ export class PlansPage {
   private geoJSON = inject(GeoJSONService);
 
   isMobile = this.platform.is('mobile') || this.platform.is('android') || this.platform.is('ios');
-  items = this.geoJSON.metadata;
+
+  sortValue = signal<'name' | 'date'>('date');
+  items = computed(() => {
+    const sorter = sortFunctions[this.sortValue()];
+    return sorter(this.geoJSON.metadata());
+  });
 
   /**
    * Log GPX filenames when files are dropped in the dropzone
@@ -34,12 +55,44 @@ export class PlansPage {
   async onFileDrop(files: NgxFileDropEntry[]) {
     for (const { fileEntry, relativePath } of files) {
       const geojson = await toGeoJSON(fileEntry);
-      const metadata: GeoJSONItem = {
-        id: generateShortRandomId(),
-        name: relativePath,
-        date: Date.now(),
-      };
+      const metadata: GeoJSONItem = { id: generateShortRandomId(), name: relativePath, date: Date.now() };
       await this.geoJSON.save(metadata, geojson);
     }
   }
+
+  /**
+   * Hånderer sorting når bruker endrer valg i select
+   */
+  onSortChange(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    const value = select.value as 'name' | 'date';
+    this.sortValue.set(value);
+  }
 }
+
+/**
+ * Sorterer basert på navn, alfabetisk
+ */
+export function sortByName(items: GeoJSONItem[]) {
+  return items.toSorted((a, b) => a.name.localeCompare(b.name));
+}
+
+/**
+ * Sorterer basert på dato, nyeste først
+ */
+export function sortByDate(items: GeoJSONItem[]) {
+  return items.toSorted((a, b) => {
+    if (a.date && b.date) {
+      return b.date - a.date;
+    }
+    if (!a.date && !b.date) {
+      return 0;
+    }
+    return a.date ? -1 : 1;
+  });
+}
+
+const sortFunctions = {
+  name: sortByName,
+  date: sortByDate,
+} as const;
