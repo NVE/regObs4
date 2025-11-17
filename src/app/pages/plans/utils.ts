7f1828f9149bd6193330431ffa0c319e27f4dab8
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2025-11-17 13:47:54 +0100

    RO-3031: Import av GeoJSON-filer (#867)

--- src/app/pages/plans/utils.ts
+++ src/app/pages/plans/utils.ts
@@ -1,9 +1,20 @@
 import { FileSystemEntry } from 'ngx-file-drop';
+import { toGeoJSON as gpxToGeoJSON } from './gpx';
+import { FeatureCollection } from 'geojson';
+import { check } from '@placemarkio/check-geojson';
 
 export function assertIsFileEntry(fileEntry: FileSystemEntry): asserts fileEntry is FileSystemFileEntry {
   if (!fileEntry.isFile) {
     throw new Error('fileEntry is not a file');
   }
+}
+
+export function isGpxFile(fileEntry: FileSystemEntry): boolean {
+  return fileEntry.name.toLowerCase().endsWith('.gpx');
+}
+
+function isGeoJSONFile(fileEntry: FileSystemEntry): boolean {
+  return fileEntry.name.toLowerCase().endsWith('.geojson') || fileEntry.name.toLowerCase().endsWith('.json');
 }
 
 export function toText(fileEntry: FileSystemFileEntry): Promise<string> {
@@ -24,4 +35,17 @@ export function generateShortRandomId(length = 6) {
   return Math.random()
     .toString(36)
     .substring(2, length + 2);
+}
+
+// leser en GPX- eller GeoJSON-fil og returnerer et GeoJSON-objekt
+export async function toGeoJSON(fileEntry: FileSystemEntry): Promise<FeatureCollection> {
+  assertIsFileEntry(fileEntry);
+  if (isGpxFile(fileEntry)) {
+    return await gpxToGeoJSON(fileEntry);
+  }
+  if (isGeoJSONFile(fileEntry)) {
+    const fileContent = await toText(fileEntry);
+    return check(fileContent) as FeatureCollection; // hvis denne blir for kresen, kan vi prøve scavenge i stedet
+  }
+  throw new Error('Unsupported file type. Only GPX and GeoJSON files are supported.');
 }
