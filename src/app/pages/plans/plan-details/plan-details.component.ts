a545a7ba506c45414d50b0d1435de18db17bb28c
marcin <martin.joodd@gmail.com>
2025-11-18 20:26:05 +0100

    added basic form

--- src/app/pages/plans/plan-details/plan-details.component.ts
+++ src/app/pages/plans/plan-details/plan-details.component.ts
@@ -1,6 +1,14 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { GeoJSONService } from 'src/app/core/services/geojson/geojson.service';
-
+import { StaticMapImageComponent } from 'src/app/modules/static-map-image/static-map-image.component';
+import { DatePipe } from '@angular/common';
+import 'nve-designsystem/components/nve-icon/nve-icon.component.js';
+import 'nve-designsystem/components/nve-menu-item/nve-menu-item.component.js';
+import 'nve-designsystem/components/nve-button/nve-button.component.js';
+import 'nve-designsystem/components/nve-input/nve-input.component.js';
+import 'nve-designsystem/components/nve-textarea/nve-textarea.component.js';
+import 'nve-designsystem/components/nve-switch/nve-switch.component.js';
+import 'nve-designsystem/components/nve-tag/nve-tag.component.js';
 /**
  * Detaljer for en turplan.
  * Ideen er at denne kan brukes både på /plans/:id, og kunne brukes i en modal som åpnes fra kartet, når man
@@ -10,10 +18,15 @@ import { GeoJSONService } from 'src/app/core/services/geojson/geojson.service';
   selector: 'app-plan-details',
   templateUrl: './plan-details.component.html',
   styleUrls: ['./plan-details.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [StaticMapImageComponent, DatePipe],
 })
 export class PlanDetailsComponent {
   geoJSON = inject(GeoJSONService);
   id = input.required<string>();
 
-  metadata = computed(() => this.geoJSON.metadata().find((m) => m.id === this.id()));
+  metadata = computed(() => {
+    const a = this.geoJSON.metadata().find((m) => m.id === this.id());
+    return a;
+  });
 }
