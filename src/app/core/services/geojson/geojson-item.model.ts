bfa0596bcbf34ce4f09f32e47bc4fc4b43b376d0
Øystein Myhre <omy@nve.no>
2025-12-03 16:35:34 +0100

    Gjort date obligatorisk og dokumentert felter

--- src/app/core/services/geojson/geojson-item.model.ts
+++ src/app/core/services/geojson/geojson-item.model.ts
@@ -1,6 +1,16 @@
+/**
+ * Metadata for et GeoJSON-objekt lagret i databasen.
+ */
 export interface GeoJSONItem {
+  /** Unik ID for geojson-objektet */
   id: string;
+
+  /** Navn på geojson-objektet */
   name: string;
-  date?: number;
+
+  /** Sist oppdatert tidspunkt i ms */
+  date: number;
+
+  /** Om objektet skal vises i kartet */
   on?: boolean;
 }
