6380235a543bc4be4fff48ee9d2edf60b6661867
Jørgen Loe Kvalberg <jolokv@nve.no>
2025-12-08 13:34:08 +0100

    RO-3034: Detaljside (#870)
    Co-authored-by: marcin <martin.joodd@gmail.com>

---
+++ src/app/pages/plans/plan/plan.page.ts
@@ -0,0 +1,62 @@
+import { Component, inject, input, CUSTOM_ELEMENTS_SCHEMA, computed, linkedSignal } from '@angular/core';
+import { IonToolbar, IonContent, IonBackButton, IonTitle, IonHeader, IonButtons } from '@ionic/angular/standalone';
+import { GeoJSONService } from 'src/app/core/services/geojson/geojson.service';
+import { DatePipe } from '@angular/common';
+import 'nve-designsystem/components/nve-icon/nve-icon.component.js';
+import 'nve-designsystem/components/nve-menu-item/nve-menu-item.component.js';
+import 'nve-designsystem/components/nve-button/nve-button.component.js';
+import 'nve-designsystem/components/nve-input/nve-input.component.js';
+import 'nve-designsystem/components/nve-textarea/nve-textarea.component.js';
+import 'nve-designsystem/components/nve-switch/nve-switch.component.js';
+import 'nve-designsystem/components/nve-tag/nve-tag.component.js';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-plan.page',
+  templateUrl: './plan.page.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [DatePipe, IonContent, IonToolbar, IonBackButton, IonTitle, IonHeader, IonButtons, TranslatePipe],
+  styleUrl: './plan.page.css',
+})
+export class PlanPage {
+  geoJSON = inject(GeoJSONService);
+  router = inject(Router);
+  id = input.required<string>();
+
+  itemMetadata = computed(() => this.geoJSON.metadata().find((m) => m.id === this.id()));
+
+  name = linkedSignal<string>(() => this.itemMetadata()?.name || '');
+  comment = linkedSignal<string>(() => this.itemMetadata()?.comment || '');
+  visibleOnMap = linkedSignal<boolean>(() => this.itemMetadata()?.visibleOnMap || false);
+
+  onNameChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.name.set(value);
+  }
+
+  onCommentChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.comment.set(value);
+  }
+  onVisibleOnMapChange(event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.visibleOnMap.set(checked);
+  }
+
+  async onRemove() {
+    await this.geoJSON.remove(this.id());
+    await this.router.navigate(['/plans']);
+  }
+
+  onSave() {
+    const itemToUpdate = {
+      id: this.id(),
+      name: this.name(),
+      comment: this.comment(),
+      visibleOnMap: this.visibleOnMap(),
+      date: Date.now(),
+    };
+    this.geoJSON.updateMetadata(itemToUpdate);
+  }
+}
