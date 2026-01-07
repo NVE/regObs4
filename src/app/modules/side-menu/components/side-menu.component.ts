35fd5d647ae388fb2413725ed3449379bf50df5f
marcin <martin.joodd@gmail.com>
2026-01-07 16:37:47 +0100

    test error

--- src/app/modules/side-menu/components/side-menu.component.ts
+++ src/app/modules/side-menu/components/side-menu.component.ts
@@ -193,4 +193,8 @@ export class SideMenuComponent implements OnInit, OnDestroy {
     }
     this.contact('MENU.CONTACT_REGOBS_ERROR', 'MENU.ERROR_REPORT_DESCRIPTION', additionalSubjectText);
   }
+
+  test() {
+    throw new Error('Test error from side menu');
+  }
 }
