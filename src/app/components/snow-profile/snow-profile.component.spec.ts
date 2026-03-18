df3dd4f1f5915414e6ef1653caf5c0598ed8c388
Jørgen Loe Kvalberg <jolokv@nve.no>
2026-03-18 21:16:52 +0100

    Fiks test

--- src/app/components/snow-profile/snow-profile.component.spec.ts
+++ src/app/components/snow-profile/snow-profile.component.spec.ts
@@ -1,22 +1,29 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SnowProfileComponent } from './snow-profile.component';
+import { provideTranslateService } from '@ngx-translate/core';
+import { KdvService } from 'src/app/modules/common-registration/registration.services';
+import { of } from 'rxjs';
+import { SnowProfileEditModel } from 'src/app/modules/common-regobs-api';
 
 describe('SnowProfileComponent', () => {
   let component: SnowProfileComponent;
   let fixture: ComponentFixture<SnowProfileComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [SnowProfileComponent],
-      imports: [IonicModule.forRoot()],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        provideTranslateService(),
+        { provide: KdvService, useValue: { getKdvRepositoryByKeyObservable: () => of([]) } },
+      ],
+      imports: [SnowProfileComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SnowProfileComponent);
+    fixture.componentRef.setInput('profile', {} as SnowProfileEditModel);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    await fixture.whenStable();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
