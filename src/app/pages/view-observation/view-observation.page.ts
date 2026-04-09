07b77215be70ffabe905a19f21bcffe56c760853
Jørgen Loe Kvalberg <jolokv@nve.no>
2026-04-09 12:51:47 +0200

    Sett litt dypere zoom

--- src/app/pages/view-observation/view-observation.page.ts
+++ src/app/pages/view-observation/view-observation.page.ts
@@ -169,7 +169,7 @@ export class ViewObservationPage extends NgDestoryBase implements OnInit {
       return undefined;
     }
     const { Latitude, Longitude } = reg.ObsLocation;
-    return `https://iskart.no?LAT=${Latitude};LON=${Longitude};ZOOM=13`;
+    return `https://iskart.no?LAT=${Latitude};LON=${Longitude};ZOOM=15`;
   });
 
   errorMesage = computed(() => {
