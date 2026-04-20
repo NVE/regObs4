ae39d03fff870df2f4dc5faacf52aba26ccaf2a7
Jørgen Loe Kvalberg <jolokv@nve.no>
2026-04-20 14:32:59 +0200

    Remove unneeded changes

--- src/app/modules/registration/components/failed-registration/failed-registration.component.ts
+++ src/app/modules/registration/components/failed-registration/failed-registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, ChangeDetectionStrategy, output } from '@angular/core';
+import { Component, inject, input, ChangeDetectionStrategy } from '@angular/core';
 import { SyncStatus } from 'src/app/modules/common-registration/registration.models';
 import { EmailComposer, EmailComposerOptions } from '@awesome-cordova-plugins/email-composer/ngx';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
@@ -42,7 +42,6 @@ export class FailedRegistrationComponent {
   private platform = inject(Platform);
 
   readonly draft = input.required({ transform: (draft: RegistrationDraft) => draft as RegistrationDraftWithError });
-  editOpened = output();
 
   constructor() {
     addIcons({ mail, create });
@@ -86,7 +85,6 @@ export class FailedRegistrationComponent {
       ...this.draft(),
       syncStatus: SyncStatus.Draft,
     });
-    this.editOpened.emit();
   }
 
   async sendEmail() {
